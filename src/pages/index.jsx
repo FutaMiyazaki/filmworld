29c9969883a86afc248feac144c323f13954e44e
capcap-jp <miyamiyacap@gmail.com>
2022-01-11 22:54:59 +0900

    ✨ カウンターを設置してuseStateでカウントアップする

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,27 @@ import styles from 'src/styles/Home.module.css';
 import { Header } from 'src/components/Layout/Header';
 import { Footer } from 'src/components/Layout/Footer';
 import { Main } from 'src/components/Main';
+import { useState } from 'react';
 
 export default function Home() {
+  const [count, setCount] = useState(1);
+  const handleClick = (e) => {
+    setCount(count => count + 1);
+  }
+
   return (
     <div className={styles.container}>
       <Head>
         <title>react app by miya</title>
       </Head>
       <Header />
+      <h1>{count}</h1>
+      <button
+        href='/about'
+        onClick={handleClick}
+      >
+        ボタン
+      </button>
       <Main page="index" />
       <Footer />
     </div>
