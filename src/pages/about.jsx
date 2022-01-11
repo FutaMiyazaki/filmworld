0915503c09503eef6064450e7de232d0feaa9c4c
capcap-jp <miyamiyacap@gmail.com>
2022-01-11 20:23:36 +0900

    🏗️ 拡張子・ディレクトリ構成・インポート方法の変更、_appを用いた処理共通化

---
+++ src/pages/about.jsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import styles from 'src/styles/Home.module.css'
+import { Header } from 'src/components/Layout/Header'
+import { Footer } from 'src/components/Layout/Footer'
+import { Main } from 'src/components/Main'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>react app by miya about</title>
+      </Head>
+      <Header />
+      <Main page="about" />
+      <Footer />
+    </div>
+  )
+}
