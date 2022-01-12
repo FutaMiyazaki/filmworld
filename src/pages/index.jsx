2e4a8af100deba702d713e57c60fd81b0d2c4024
capcap-jp <miyamiyacap@gmail.com>
2022-01-13 00:30:26 +0900

    ♻️ 状態を変更するときにprevをつけて名前をわかりやすくした

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,7 +13,7 @@ export default function Home() {
   const handleClick = useCallback(
     (e) => {
       if (count < 10) {
-        setCount((count) => count + 1);
+        setCount((prevCount) => prevCount + 1);
       }
     },
     [count]
@@ -36,7 +36,7 @@ export default function Home() {
   }, []);
 
   const handleDisplay = useCallback(() => {
-    setIsShow((isShow) => !isShow);
+    setIsShow((prevIsShow) => !prevIsShow);
   }, []);
 
   return (
