e97f0027ed7a311fa23c242face04dc2e243c1a5
capcap-jp <miyamiyacap@gmail.com>
2022-01-14 14:59:34 +0900

    ✨ ユーザーのfetch、fetcherの共通化

--- src/pages/post/[id]/index.jsx
+++ src/pages/post/[id]/index.jsx
@@ -1,19 +1,12 @@
-import Head from "next/head";
 import styles from "src/styles/Home.module.css";
 import { Header } from "src/components/Layout/Header";
-import { useRouter } from "next/router";
+import { Post } from "src/components/Post";
 
 export default function PostId() {
-  const router = useRouter();
-  console.log(router);
-
   return (
     <div className={styles.container}>
-      <Head>
-        <title>indexページ</title>
-      </Head>
       <Header />
-      <div>{router.query.id}</div>
+      <Post />
     </div>
   );
 }
