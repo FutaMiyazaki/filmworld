2847f30f50ecb2cb1bd653944c0573df00b8bc70
capcap-jp <miyamiyacap@gmail.com>
2022-01-14 15:16:55 +0900

    🔥 不必要なコードやファイルを削除

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,20 +1,13 @@
 import "src/styles/globals.css";
 import Head from "next/head";
-import { useCounter } from "src/hooks/useCounter";
-import { useInputArray } from "src/hooks/useInputArray";
-import { useBgLightBlue } from "src/hooks/useBgLightBlue";
 
 function MyApp({ Component, pageProps }) {
-  const counter = useCounter();
-  const inputArray = useInputArray();
-  useBgLightBlue();
-
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} {...counter} {...inputArray} />
+      <Component {...pageProps} />
     </>
   );
 }
