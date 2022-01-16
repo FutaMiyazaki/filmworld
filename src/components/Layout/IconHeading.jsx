01816419c01dd8ef0a8046ba4f6b4086ca3d776f
capcap-jp <miyamiyacap@gmail.com>
2022-01-17 03:08:08 +0900

    ✨ アイコン付きの見出しを作成

---
+++ src/components/Layout/IconHeading.jsx
@@ -0,0 +1,21 @@
+import { createElement } from "react";
+import { Grid, Typography } from "@mui/material";
+import * as Icons from "@mui/icons-material";
+
+export function IconHeading(props) {
+  return (
+    <Grid container direction="row" alignItems="center" sx={{ mb: 2 }}>
+      <Grid item sx={{ mr: 1 }}>
+        {createElement(Icons[props.icon], {
+          fontSize: "large",
+          color: "primary",
+        })}
+      </Grid>
+      <Grid item>
+        <Typography component="div" variant="h4" sx={{ color: "white" }}>
+          {props.text}
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+}
