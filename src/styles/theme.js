6f6b6a0c143cb875d161af5ef94be48708631942
capcap-jp <miyamiyacap@gmail.com>
2022-01-16 23:26:45 +0900

    🎨 muiのデフォルトテーマ設定ファイルを作成

---
+++ src/styles/theme.js
@@ -0,0 +1,46 @@
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#69D2E7",
+      light: "#B1E2EC",
+      dark: "#51A4B5",
+    },
+    secondary: {
+      main: "#FA6900",
+      light: "#FDAD0D",
+      dark: "#E3460B",
+    },
+    common: {
+      black: "#343a40",
+      white: "#F4F4F4",
+    },
+    info: {
+      main: "#005CAF",
+      light: "#0087FC",
+      dark: "#00437D",
+    },
+    success: {
+      main: "#00AA90",
+      light: "#00F7D2",
+      dark: "#007866",
+    },
+    warning: {
+      main: "#FFB11B",
+      light: "#FFDB0F",
+      dark: "#E8820E",
+    },
+    error: {
+      main: "#CB1B45",
+      light: "#D55B78",
+      dark: "#991433",
+    },
+    tonalOffset: 0.2,
+    background: {
+      default: "#0A192A",
+    },
+  },
+});
+
+export default responsiveFontSizes(theme);
