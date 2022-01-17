bbb9850f0673a2b757938553273ff662c761c033
capcap-jp <miyamiyacap@gmail.com>
2022-01-17 23:13:12 +0900

    ♻️ Bottonコンポーネント利用に変更

--- src/components/Layout/Link/ExternalLink.jsx
+++ src/components/Layout/Link/ExternalLink.jsx
@@ -1,30 +1,17 @@
-import { Grid, Link as MuiLink, Typography } from "@mui/material";
+import { Link as MuiLink, Button } from "@mui/material";
 import LinkIcon from "@mui/icons-material/Link";
 
 export function ExternalLink(props) {
   return (
-    <Grid sx={{ m: 1 }}>
-      <MuiLink
-        href={props.url}
-        target="_blank"
-        rel="noopener noreferrer"
-        underline="hover"
-      >
-        <Grid container direction="row" alignItems="center">
-          <Grid item sx={{ mr: 1 }}>
-            <LinkIcon color="primary" />
-          </Grid>
-          <Grid item>
-            <Typography
-              component="div"
-              variant="subtitle1"
-              sx={{ color: "primary" }}
-            >
-              {props.text}
-            </Typography>
-          </Grid>
-        </Grid>
-      </MuiLink>
-    </Grid>
+    <MuiLink
+      href={props.url}
+      target="_blank"
+      rel="noopener noreferrer"
+      underline="hover"
+    >
+      <Button variant="text" color="primary" startIcon={<LinkIcon />}>
+        {props.text}
+      </Button>
+    </MuiLink>
   );
 }
