f0600e5c17e071a03febe38a9a75b99c44fd8540
capcap-jp <miyamiyacap@gmail.com>
2022-01-17 23:15:00 +0900

    ✨ 見出しを追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,14 @@
 import Head from "next/head";
 import { Movies } from "src/components/Movies/Movies";
+import { IconHeading } from "src/components/Layout/IconHeading";
 
-export default function Index(props) {
+export default function Index() {
   return (
     <div>
       <Head>
         <title>トップページ</title>
       </Head>
+      <IconHeading icon="TrendingUp" text="人気の映画" />
       <Movies />
     </div>
   );
