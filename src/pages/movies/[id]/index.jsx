c1850d2833f13bc618375b63796c8366a5b8cbf1
capcap-jp <miyamiyacap@gmail.com>
2022-01-19 04:18:46 +0900

    ♻️ レスポンシブデザインを修正、一部コンポーネントを切り出し

--- src/pages/movies/[id]/index.jsx
+++ src/pages/movies/[id]/index.jsx
@@ -12,12 +12,16 @@ import { useMediaQuery } from "react-responsive";
 import { useMovie } from "src/hooks/useMovie";
 import { Loading } from "src/components/Layout/Loading";
 import { ExternalLink } from "src/components/Layout/Link/ExternalLink";
+import { InfoHeader } from "src/components/Movies/InfoHeader";
+import { PaperText } from "src/components/Layout/PaperText";
+import { UserScore } from "src/components/Movies/UserScore";
+import { TitleHeader } from "src/components/Movies/TitleHeader";
 
 export default function MoviesId() {
-  const { data, error, isLoading } = useMovie();
+  const { movieInfo, movieCredits, error, isLoading } = useMovie();
   const isMobileScreen = useMediaQuery({ query: "(max-width: 600px)" });
-  const releaseDate = data?.release_date.replace(/-/g, "/");
-  console.log(data);
+  const releaseDate = movieInfo?.release_date.replace(/-/g, "/");
+  console.log(movieCredits);
 
   if (isLoading) {
     return <Loading />;
@@ -29,60 +33,52 @@ export default function MoviesId() {
   return (
     <div>
       <Head>
-        <title>{data?.title}</title>
+        <title>{movieInfo?.title}</title>
       </Head>
       <Grid container columns={{ xs: 5, md: 12 }} spacing={2}>
-        <Grid item xs={5} sx={{ display: { xs: "block", sm: "none" }, mb: 1 }}>
-          <Typography
-            variant="subtitle1"
-            color="white"
-            sx={{ fontWeight: "bold" }}
-          >
-            {data.title}
-          </Typography>
-          <Typography variant="subtitle2" color="gray">
-            {data.original_title}
-          </Typography>
-        </Grid>
+        <TitleHeader
+          xsDisplay="block"
+          smDisplay="none"
+          title={movieInfo?.title}
+          originalTitle={movieInfo?.original_title}
+        />
         <Grid item xs={2} sm={2} md={4}>
           {isMobileScreen ? (
-            <Card sx={{ mb: 1, bgcolor: "#0A192A" }}>
+            <Card sx={{ mb: 1 }}>
               <CardMedia
                 component="img"
                 sx={{
                   height: "30vh",
                 }}
-                image={`https://image.tmdb.org/t/p/w185${data.poster_path}`}
+                image={`https://image.tmdb.org/t/p/w185${movieInfo?.poster_path}`}
                 alt="ポスター画像"
               />
             </Card>
           ) : (
-            <Card sx={{ mb: 1, bgcolor: "#0A192A" }}>
+            <Card sx={{ mb: 1 }}>
               <CardMedia
                 component="img"
                 sx={{
                   width: "30vh",
                 }}
-                image={`https://image.tmdb.org/t/p/w185${data.poster_path}`}
+                image={`https://image.tmdb.org/t/p/w185${movieInfo?.poster_path}`}
                 alt="ポスター画像"
               />
             </Card>
           )}
-          <ExternalLink url={data.homepage} text="公式サイト" />
+          <ExternalLink url={movieInfo?.homepage} text="公式サイト" />
         </Grid>
         <Grid item xs={3} md={8}>
-          <Box sx={{ display: { xs: "none", sm: "block" } }}>
-            <Typography variant="h4" color="white" sx={{ fontWeight: "bold" }}>
-              {data.title}
-            </Typography>
-            <Typography variant="subtitle1" color="gray" sx={{ mb: 2 }}>
-              {data.original_title}
-            </Typography>
-          </Box>
+          <TitleHeader
+            xsDisplay="none"
+            smDisplay="block"
+            title={movieInfo?.title}
+            originalTitle={movieInfo?.original_title}
+          />
           <Typography variant="body1" color="white" sx={{ mb: 1 }}>
             公開日: {releaseDate}
           </Typography>
-          {data.genres.map((genre) => {
+          {movieInfo?.genres.map((genre) => {
             return (
               <Chip
                 key={genre.id}
@@ -94,43 +90,88 @@ export default function MoviesId() {
               />
             );
           })}
-          <Box sx={{ display: { xs: "none", sm: "block" }, my: 1 }}>
-            <Typography
-              variant="subtitle1"
-              color="white"
-              sx={{ fontWeight: "bold" }}
-            >
-              あらすじ
-            </Typography>
-            <Typography variant="body2" color="white" sx={{ mb: 2 }}>
-              {data.overview}
-            </Typography>
-          </Box>
-          <Typography
-            variant="subtitle1"
-            color="white"
-            sx={{ fontWeight: "bold" }}
-          >
-            制作会社
-          </Typography>
-          {data.production_companies.map((company) => {
-            return (
-              <Paper
-                key={company.id}
-                sx={{
-                  display: "inline-block",
-                  textAlign: "center",
-                  px: 1,
-                  mr: 1,
-                  mb: 1,
-                }}
+          {!isMobileScreen && <UserScore score={movieInfo?.vote_average} />}
+          {movieInfo?.overview && (
+            <Box sx={{ display: { xs: "none", sm: "block" }, my: 1 }}>
+              <Typography
+                variant="subtitle1"
+                color="white"
+                sx={{ fontWeight: "bold" }}
               >
-                <Typography variant="body2">{company.name}</Typography>
-              </Paper>
-            );
-          })}
+                あらすじ
+              </Typography>
+              <Typography variant="body2" color="white" sx={{ mb: 2 }}>
+                {movieInfo?.overview}
+              </Typography>
+            </Box>
+          )}
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            <InfoHeader text="制作会社" />
+            {movieInfo?.production_companies.map((company) => {
+              return (
+                <PaperText key={company.id}>
+                  <Typography variant="body2">{company.name}</Typography>
+                </PaperText>
+              );
+            })}
+            <InfoHeader text="監督" />
+            {movieCredits?.crew.map((crew) => {
+              return crew.job === "Director" ? (
+                <Paper
+                  key={crew.id}
+                  sx={{
+                    display: "inline-block",
+                    textAlign: "center",
+                    px: 1,
+                    mr: 1,
+                    mb: 1,
+                  }}
+                >
+                  <Typography variant="body2">{crew.name}</Typography>
+                </Paper>
+              ) : null;
+            })}
+            <InfoHeader text="脚本" />
+            {movieCredits?.crew.map((crew) => {
+              return crew.job ===
+                ("Original Film Writer" || "Story" || "Writing") ? (
+                <Paper
+                  key={crew.id}
+                  sx={{
+                    display: "inline-block",
+                    textAlign: "center",
+                    px: 1,
+                    mr: 1,
+                    mb: 1,
+                  }}
+                >
+                  <Typography variant="body2">{crew.name}</Typography>
+                </Paper>
+              ) : null;
+            })}
+            <InfoHeader text="出演者" />
+            {movieCredits?.cast.map((cast, i) => {
+              return i < 10 ? (
+                <Paper
+                  key={cast.id}
+                  sx={{
+                    display: "inline-block",
+                    textAlign: "center",
+                    px: 1,
+                    mr: 1,
+                    mb: 1,
+                  }}
+                >
+                  <Typography variant="body2">{cast.name}</Typography>
+                </Paper>
+              ) : null;
+            })}
+          </Box>
         </Grid>
-        {data.overview && (
+        <Grid item sx={{ display: { xs: "block", sm: "none" } }}>
+          <UserScore score={movieInfo?.vote_average} />
+        </Grid>
+        {movieInfo?.overview && (
           <Grid item sx={{ display: { xs: "block", sm: "none" } }}>
             <Typography
               variant="subtitle1"
@@ -140,10 +181,72 @@ export default function MoviesId() {
               あらすじ
             </Typography>
             <Typography variant="body2" color="white">
-              {data.overview}
+              {movieInfo?.overview}
             </Typography>
           </Grid>
         )}
+        <Grid item sx={{ display: { xs: "block", sm: "none" } }}>
+          <InfoHeader text="制作会社" />
+          {movieInfo?.production_companies.map((company) => {
+            return (
+              <PaperText key={company.id}>
+                <Typography variant="body2">{company.name}</Typography>
+              </PaperText>
+            );
+          })}
+          <InfoHeader text="監督" />
+          {movieCredits?.crew.map((crew) => {
+            return crew.job === "Director" ? (
+              <Paper
+                key={crew.id}
+                sx={{
+                  display: "inline-block",
+                  textAlign: "center",
+                  px: 1,
+                  mr: 1,
+                  mb: 1,
+                }}
+              >
+                <Typography variant="body2">{crew.name}</Typography>
+              </Paper>
+            ) : null;
+          })}
+          <InfoHeader text="脚本" />
+          {movieCredits?.crew.map((crew) => {
+            return crew.job ===
+              ("Original Film Writer" || "Story" || "Writing") ? (
+              <Paper
+                key={crew.id}
+                sx={{
+                  display: "inline-block",
+                  textAlign: "center",
+                  px: 1,
+                  mr: 1,
+                  mb: 1,
+                }}
+              >
+                <Typography variant="body2">{crew.name}</Typography>
+              </Paper>
+            ) : null;
+          })}
+          <InfoHeader text="出演者" />
+          {movieCredits?.cast.map((cast, i) => {
+            return i < 10 ? (
+              <Paper
+                key={cast.id}
+                sx={{
+                  display: "inline-block",
+                  textAlign: "center",
+                  px: 1,
+                  mr: 1,
+                  mb: 1,
+                }}
+              >
+                <Typography variant="body2">{cast.name}</Typography>
+              </Paper>
+            ) : null;
+          })}
+        </Grid>
       </Grid>
     </div>
   );
