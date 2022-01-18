185b1f007734c14e3b4564b50127d699c8b63480
capcap-jp <miyamiyacap@gmail.com>
2022-01-19 04:15:13 +0900

    ✨ クレジット情報を取得できるように変更

--- src/hooks/useMovie.jsx
+++ src/hooks/useMovie.jsx
@@ -2,17 +2,27 @@ import { useRouter } from "next/router";
 import { fetcher } from "src/utils/fetcher";
 import useSWR from "swr";
 
-export const useMovie = () => {
+export function useMovie() {
   const router = useRouter();
-  const { data, error } = useSWR(
+  const { data: movieInfo, error: movieInfoError } = useSWR(
     router.query.id
       ? `https://api.themoviedb.org/3/movie/${router.query.id}?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&language=ja-JP`
       : null,
     fetcher
   );
+
+  const { data: movieCredits, error: movieCreditsError } = useSWR(
+    router.query.id
+      ? `https://api.themoviedb.org/3/movie/${router.query.id}/credits?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&language=ja-JP`
+      : null,
+    fetcher
+  );
+
   return {
-    data,
-    error,
-    isLoading: !error && !data,
+    movieInfo,
+    movieCredits,
+    error: movieInfoError || movieCreditsError,
+    isLoading:
+      !movieInfo && !movieCredits && !movieInfoError && !movieCreditsError,
   };
-};
+}
