6d0dde62493773b1ffe7251da9c4934d02e85a4f
capcap-jp <miyamiyacap@gmail.com>
2022-01-20 19:45:07 +0900

    ♻️ 表示デザインを変更

--- src/components/Movies/Card/MobileCard.jsx
+++ src/components/Movies/Card/MobileCard.jsx
@@ -8,37 +8,53 @@ import {
   Typography,
 } from "@mui/material";
 import BasicLink from "src/components/Layout/Link/BasicLink";
+import { UserScore } from "src/components/Movies/UserScore";
 
 export function MobileCard(props) {
   return (
-    <Card sx={{ display: "flex" }}>
-      <NextLink passHref href={`/movies/${props.movie.id}`}>
-        <MuiLink underline="none">
-          <CardMedia
-            component="img"
-            sx={{ width: 150 }}
-            image={`https://image.tmdb.org/t/p/w185${props.movie.poster_path}`}
-            alt="ポスター画像"
-          />
-        </MuiLink>
-      </NextLink>
-      <Box sx={{ display: "flex", flexDirection: "column" }}>
-        <CardContent sx={{ flex: "1 0 auto" }}>
+    <Card>
+      <CardContent sx={{ pa: 1 }}>
+        <Box sx={{ mb: 1 }}>
           <BasicLink
             href={{ pathname: `/movies/${props.movie.id}` }}
             tag="subtitle1"
             text={props.movie.title}
           />
-          <div style={{ flexGrow: 1 }}></div>
-          <Typography
-            variant="overline"
-            color="white"
-            sx={{ display: "inline" }}
-          >
-            {props.movie?.release_date.replace(/-/g, "/")}
-          </Typography>
-        </CardContent>
-      </Box>
+        </Box>
+        <Box sx={{ display: "flex" }}>
+          <Box>
+            <NextLink passHref href={`/movies/${props.movie.id}`}>
+              <MuiLink underline="none">
+                <CardMedia
+                  component="img"
+                  sx={{ width: 100 }}
+                  image={`https://image.tmdb.org/t/p/w185${props.movie.poster_path}`}
+                  alt="ポスター画像"
+                />
+              </MuiLink>
+            </NextLink>
+          </Box>
+          <Box sx={{ ml: 2 }}>
+            <Typography
+              variant="subtitle2"
+              color="white"
+              sx={{ display: "inline" }}
+            >
+              公開日: {props.movie.release_date.replace(/-/g, "/")}
+            </Typography>
+            <UserScore
+              voteAverage={props.movie.vote_average}
+              voteCount={props.movie.vote_count}
+              size="small"
+            />
+            <BasicLink
+              href={{ pathname: `/movies/${props.movie.id}` }}
+              tag="subtitle2"
+              text=">>詳しい情報を見る"
+            />
+          </Box>
+        </Box>
+      </CardContent>
     </Card>
   );
 }
