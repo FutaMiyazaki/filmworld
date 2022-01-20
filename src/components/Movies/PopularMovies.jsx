774858ddb8971912040a30bcb80dc6c797ad6669
capcap-jp <miyamiyacap@gmail.com>
2022-01-20 19:49:01 +0900

    ✨ 表示するカードをコンポーネントに切り出した

---
+++ src/components/Movies/PopularMovies.jsx
@@ -0,0 +1,44 @@
+import { usePopularMovies } from "src/hooks/usePopularMovies";
+import { useMediaQuery } from "react-responsive";
+import { Loading } from "src/components/Layout/Loading";
+import { Grid } from "@mui/material";
+import { MobileCard } from "src/components/Movies/Card/MobileCard";
+import { PcCard } from "src/components/Movies/Card/Pccard";
+
+export function PopularMovies() {
+  const isMobileScreen = useMediaQuery({ query: "(max-width: 600px)" });
+  const { data, error, isLoading, isEmpty } = usePopularMovies();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  if (isEmpty) {
+    return <div>データは空です</div>;
+  }
+
+  return (
+    <Grid
+      container
+      spacing={2}
+      justifyContent="center"
+      columns={{ xs: 4, sm: 8 }}
+    >
+      {data.results.map((movie) => {
+        return (
+          <Grid key={movie.id} item xs={4} sm={4}>
+            {isMobileScreen ? (
+              <MobileCard movie={movie} />
+            ) : (
+              <PcCard movie={movie} />
+            )}
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
+}
