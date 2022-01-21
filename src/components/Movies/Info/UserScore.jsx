711510ac3762645d42647d749aad2b2b2140945a
capcap-jp <miyamiyacap@gmail.com>
2022-01-21 20:19:23 +0900

    ♻️ 表示レイアウトを修正

--- src/components/Movies/Info/UserScore.jsx
+++ src/components/Movies/Info/UserScore.jsx
@@ -1,6 +1,10 @@
 import { Box, Rating, Typography } from "@mui/material";
 
 export function UserScore(props) {
+  const shapingScore = (score) => {
+    return Math.floor((score / 2) * Math.pow(10, 1)) / Math.pow(10, 1);
+  };
+
   return (
     <Box
       sx={{
@@ -12,17 +16,17 @@ export function UserScore(props) {
       <Rating
         readOnly
         name="read-only"
-        value={props?.voteAverage / 2}
+        value={shapingScore(props?.voteAverage)}
         precision={0.1}
         size={props?.size}
       />
-      <Box sx={{ ml: 2 }}>
+      <Box sx={{ ml: 1 }}>
         <Typography
-          variant="h6"
+          variant="subtitle1"
           color="#FBBD30"
           sx={{ mr: 1, display: "inline" }}
         >
-          {props?.voteAverage / 2}
+          {shapingScore(props?.voteAverage)}
         </Typography>
         <Typography variant="caption" color="white">
           ({props?.voteCount})
