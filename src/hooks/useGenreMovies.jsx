d3fe99a58ca61f00a558a91caa411f5d703d31fb
capcap-jp <miyamiyacap@gmail.com>
2022-01-22 22:33:37 +0900

    ♻️ ページネーションの実装に伴い修正

--- src/hooks/useGenreMovies.jsx
+++ src/hooks/useGenreMovies.jsx
@@ -6,8 +6,8 @@ export const useGenreMovies = () => {
   const router = useRouter();
 
   const { data, error } = useSWR(
-    router.query.with
-      ? `https://api.themoviedb.org/3/discover/movie?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&language=ja-JP&page=1&include_adult=false&with_genres=${router.query.with}`
+    router.query.with && router.query.page
+      ? `https://api.themoviedb.org/3/discover/movie?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&language=ja-JP&page=${router.query.page}&include_adult=false&with_genres=${router.query.with}`
       : null,
     fetcher
   );
