d17069096c83a688f59554dacbaaad628261e52a
capcap-jp <miyamiyacap@gmail.com>
2022-01-23 19:59:42 +0900

    ✨ 映画検索ページをページネーションに対応

--- src/components/Movies/SearchMovies.jsx
+++ src/components/Movies/SearchMovies.jsx
@@ -1,13 +1,22 @@
-import { useSearchMovies } from "src/hooks/useSearchMovie";
+import { useState } from "react";
+import { useRouter } from "next/router";
 import { useMediaQuery } from "react-responsive";
+import { Grid, Pagination, Typography } from "@mui/material";
 import { Loading } from "src/components/Layout/Loading";
-import { Grid } from "@mui/material";
 import { MobileCard } from "src/components/Movies/Card/MobileCard";
 import { PcCard } from "src/components/Movies/Card/Pccard";
+import { useSearchMovies } from "src/hooks/useSearchMovie";
 
 export function SearchMovies() {
   const isMobileScreen = useMediaQuery({ query: "(max-width: 600px)" });
+  const router = useRouter();
+  const [page, setPage] = useState(1);
   const { data, error, isLoading, isEmpty } = useSearchMovies();
+
+  const handlePage = (e, clickPage) => {
+    setPage((page) => clickPage);
+    router.push(`/search?keyword=${router.query.keyword}&page=${clickPage}`);
+  };
 
   if (isLoading) {
     return <Loading />;
@@ -21,24 +30,53 @@ export function SearchMovies() {
     return <div>見つかりませんでした</div>;
   }
 
+  console.log(data);
+
   return (
-    <Grid
-      container
-      spacing={2}
-      justifyContent="center"
-      columns={{ xs: 4, sm: 8 }}
-    >
-      {data.results.map((movie) => {
-        return (
-          <Grid key={movie.id} item xs={4} sm={4}>
-            {isMobileScreen ? (
-              <MobileCard movie={movie} />
-            ) : (
-              <PcCard movie={movie} />
-            )}
-          </Grid>
-        );
-      })}
-    </Grid>
+    <div>
+      <Grid
+        container
+        spacing={2}
+        justifyContent="center"
+        columns={{ xs: 4, sm: 8 }}
+      >
+        <Grid item xs={4} sm={8} sx={{ mb: 2 }}>
+          <Typography
+            variant="h5"
+            color="primary"
+            sx={{ display: "inline", fontWeight: "bold" }}
+          >
+            {router.query.keyword}
+          </Typography>
+          <Typography
+            variant="h6"
+            sx={{ display: "inline", fontWeight: "bold" }}
+          >
+            の検索結果:{data.total_results}件
+          </Typography>
+        </Grid>
+        {data.results.map((movie) => {
+          return (
+            <Grid key={movie.id} item xs={4} sm={4}>
+              {isMobileScreen ? (
+                <MobileCard movie={movie} />
+              ) : (
+                <PcCard movie={movie} />
+              )}
+            </Grid>
+          );
+        })}
+      </Grid>
+      <Grid container justifyContent="center" spacing={1} sx={{ mt: 4 }}>
+        <Pagination
+          count={data.total_pages}
+          variant="outlined"
+          shape="rounded"
+          color="primary"
+          size={isMobileScreen ? "small" : "medium"}
+          onChange={handlePage}
+        />
+      </Grid>
+    </div>
   );
 }
--- src/hooks/useSearchMovie.jsx
+++ src/hooks/useSearchMovie.jsx
@@ -6,7 +6,9 @@ export const useSearchMovies = () => {
   const router = useRouter();
   const query = router.query;
   const { data, error } = useSWR(
-    `https://api.themoviedb.org/3/search/movie?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&query=${query.keyword}&language=ja-JP&page=1&include_adult=false`,
+    router.query.page && router.query.keyword
+      ? `https://api.themoviedb.org/3/search/movie?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&page=${router.query.page}&query=${query.keyword}&language=ja-JP&include_adult=false`
+      : null,
     fetcher
   );
 
