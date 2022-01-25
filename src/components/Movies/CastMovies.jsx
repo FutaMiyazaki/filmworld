f7ec8c98a9e404c5b04b13d486ebb58322596223
capcap-jp <miyamiyacap@gmail.com>
2022-01-25 16:07:37 +0900

    ✨ 映画一覧のソート機能を追加

--- src/components/Movies/CastMovies.jsx
+++ src/components/Movies/CastMovies.jsx
@@ -1,24 +1,42 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { useMediaQuery } from "react-responsive";
-import { Grid, Pagination } from "@mui/material";
+import {
+  FormControl,
+  FormHelperText,
+  Grid,
+  MenuItem,
+  Pagination,
+  Select,
+} from "@mui/material";
 import { Loading } from "src/components/Layout/Loading";
+import { PageHeading } from "src/components/Layout/PageHeading";
 import { MobileCard } from "src/components/Movies/Card/MobileCard";
 import { PcCard } from "src/components/Movies/Card/Pccard";
 import { useCastMovies } from "src/hooks/useCastMovies";
-import { PageHeading } from "src/components/Layout/PageHeading";
 
 export function CastMovies() {
-  const isMobileScreen = useMediaQuery({ query: "(max-width: 600px)" });
-  const { data, error, isLoading } = useCastMovies();
-  const [page, setPage] = useState(1);
-
   const router = useRouter();
+  const isMobileScreen = useMediaQuery({ query: "(max-width: 600px)" });
+  const [page, setPage] = useState(1);
+  const [sort, setSort] = useState("popularity.desc");
+  const { movies, cast, error, isLoading } = useCastMovies();
 
   const handlePage = (e, clickPage) => {
     setPage((page) => clickPage);
-    router.push(`/movies/cast?id=${router.query.id}&page=${clickPage}`);
+    router.push(
+      `/movies/cast?id=${router.query.id}&page=${clickPage}&sort=${router.query.sort}`
+    );
   };
+
+  const handleChange = useCallback(
+    (e) => {
+      const newSort = e.target.value;
+      setSort(newSort);
+      router.push(`/movies/cast?id=${router.query.id}&page=1&sort=${newSort}`);
+    },
+    [sort]
+  );
 
   if (isLoading) {
     return <Loading />;
@@ -30,14 +48,38 @@ export function CastMovies() {
 
   return (
     <div>
-      <PageHeading text="xxが出演している映画" />
+      <PageHeading primaryText={cast?.name} text="が出演している映画" />
+      <Grid
+        container
+        justifyContent="flex-end"
+        xs={12}
+        sm={12}
+        sx={{ mt: 2, mb: 4 }}
+      >
+        <Grid item xs="12" sm="4">
+          <FormControl fullWidth>
+            <Select
+              labelId="select-sort-label"
+              id="select-sort"
+              value={sort}
+              label="Sort"
+              onChange={handleChange}
+            >
+              <MenuItem value="popularity.desc">人気順</MenuItem>
+              <MenuItem value="release_date.desc">公開日</MenuItem>
+              <MenuItem value="revenue.desc">興行収入</MenuItem>
+            </Select>
+            <FormHelperText>ソート順</FormHelperText>
+          </FormControl>
+        </Grid>
+      </Grid>
       <Grid
         container
         spacing={2}
-        justifyContent="center"
+        justifyContent="flex-start"
         columns={{ xs: 4, sm: 8 }}
       >
-        {data.results.map((movie) => {
+        {movies?.results.map((movie) => {
           return (
             <Grid key={movie.id} item xs={4} sm={4}>
               {isMobileScreen ? (
@@ -51,11 +93,10 @@ export function CastMovies() {
       </Grid>
       <Grid container justifyContent="center" spacing={1} sx={{ mt: 3 }}>
         <Pagination
-          count={data.total_pages}
+          count={movies?.total_pages}
           variant="outlined"
           shape="rounded"
           color="primary"
-          size={isMobileScreen ? "small" : "medium"}
           onChange={handlePage}
         />
       </Grid>
--- src/hooks/useCastMovies.jsx
+++ src/hooks/useCastMovies.jsx
@@ -5,15 +5,23 @@ import useSWR from "swr";
 export const useCastMovies = () => {
   const router = useRouter();
 
-  const { data, error } = useSWR(
+  const { data: movies, error: moviesError } = useSWR(
     router.query.id && router.query.page
-      ? `https://api.themoviedb.org/3/discover/movie?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&language=ja-JP&page=${router.query.page}&include_adult=false&with_people=${router.query.id}`
+      ? `https://api.themoviedb.org/3/discover/movie?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&language=ja-JP&page=${router.query.page}&include_adult=false&with_people=${router.query.id}&sort_by=${router.query.sort}`
+      : null,
+    fetcher
+  );
+
+  const { data: cast, error: castError } = useSWR(
+    router.query.id
+      ? `https://api.themoviedb.org/3/person/${router.query.id}?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&language=ja-JP`
       : null,
     fetcher
   );
   return {
-    data,
-    error,
-    isLoading: !error && !data,
+    movies,
+    cast,
+    error: moviesError || castError,
+    isLoading: !movies && !cast && !moviesError && !castError,
   };
 };
