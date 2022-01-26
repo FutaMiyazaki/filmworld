ddcc9508cbb2f09dd4798b45d68b489366a1b830
capcap-jp <miyamiyacap@gmail.com>
2022-01-27 02:48:09 +0900

    🐛 APIエンドポイントを修正

--- src/hooks/useRevenueMovies.jsx
+++ src/hooks/useRevenueMovies.jsx
@@ -6,7 +6,7 @@ export const useRevenueMovies = () => {
   const router = useRouter();
   const { data: revenueMovies, error } = useSWR(
     router.query.page
-      ? `https://api.themoviedb.org/3/discover/movie/?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&language=ja-JP&include_adult=false&page=${router.query.page}&sort_by=revenue.desc`
+      ? `https://api.themoviedb.org/3/discover/movie?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&language=ja-JP&include_adult=false&page=${router.query.page}&sort_by=revenue.desc`
       : null,
     fetcher
   );
