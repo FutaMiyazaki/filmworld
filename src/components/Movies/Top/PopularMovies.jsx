96eb3f67fe70dabe8a57e9b1aaa68a9f55dc3ceb
capcap-jp <miyamiyacap@gmail.com>
2022-01-26 13:53:04 +0900

    🚚  トップページ用にファイル名を変更

--- src/components/Movies/Top/PopularMovies.jsx
+++ src/components/Movies/Top/PopularMovies.jsx
@@ -1,13 +1,14 @@
-import { useMoviesTop } from "src/hooks/useMoviesTop";
+import NextLink from "next/link";
 import { useMediaQuery } from "react-responsive";
+import { Grid, Link as MuiLink } from "@mui/material";
+import { usePopularMovies } from "src/hooks/topMovies/usePopularMovies";
 import { Loading } from "src/components/Layout/Loading";
-import { Grid } from "@mui/material";
 import { MobileCard } from "src/components/Movies/Card/MobileCard";
 import { PcCard } from "src/components/Movies/Card/Pccard";
 
-export function PopularMoviesTop() {
+export function PopularMovies() {
   const isMobileScreen = useMediaQuery({ query: "(max-width: 600px)" });
-  const { data, error, isLoading } = useMoviesTop();
+  const { popularMovies, error, isLoading } = usePopularMovies();
 
   if (isLoading) {
     return <Loading />;
@@ -23,9 +24,10 @@ export function PopularMoviesTop() {
       spacing={2}
       justifyContent="center"
       columns={{ xs: 4, sm: 8 }}
+      sx={{ mb: 4 }}
     >
-      {data.results.map((movie) => {
-        return (
+      {popularMovies.results.map((movie, i) => {
+        return i < 8 ? (
           <Grid key={movie.id} item xs={4} sm={4}>
             {isMobileScreen ? (
               <MobileCard movie={movie} />
@@ -33,8 +35,15 @@ export function PopularMoviesTop() {
               <PcCard movie={movie} />
             )}
           </Grid>
-        );
+        ) : null;
       })}
+      <Grid item sx={{ mt: 1 }}>
+        <NextLink href="/movies/popular?page=1" passHref>
+          <MuiLink underline="hover" sx={{ fontWeight: "bold" }}>
+            もっと見る
+          </MuiLink>
+        </NextLink>
+      </Grid>
     </Grid>
   );
 }
--- src/hooks/topMovies/usePopularMovies.jsx
+++ src/hooks/topMovies/usePopularMovies.jsx
@@ -1,14 +1,15 @@
 import { fetcher } from "src/utils/fetcher";
 import useSWR from "swr";
 
-export const useMoviesTop = () => {
-  const { data, error } = useSWR(
+export const usePopularMovies = () => {
+  const { data: popularMovies, error } = useSWR(
     "https://api.themoviedb.org/3/movie/popular?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&language=ja-JP&page=1",
     fetcher
   );
+
   return {
-    data,
+    popularMovies,
     error,
-    isLoading: !error && !data,
+    isLoading: !popularMovies && !error,
   };
 };
