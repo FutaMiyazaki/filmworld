6e2bdb10eb6c57e1a71cefea3561011d2d635ac2
capcap-jp <miyamiyacap@gmail.com>
2022-01-27 20:59:30 +0900

    ♻️ ページ見出しにジャンル名を表示するように変更

--- src/pages/genres/movies/index.jsx
+++ src/pages/genres/movies/index.jsx
@@ -1,17 +1,27 @@
+import { useCallback } from "react";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { useGenres } from "src/hooks/useGenres";
 import { PageHeading } from "src/components/Layout/PageHeading";
 import { GenreMovies } from "src/components/Movies/GenreMovies";
 
 export default function Posts() {
   const router = useRouter();
+  const { data, error, isLoading } = useGenres();
+
+  const genreName = useCallback(
+    data?.genres.filter((genre) => {
+      return genre.id == router.query.genre_id;
+    }),
+    []
+  );
 
   return (
     <div>
       <Head>
-        <title>ジャンルの映画一覧 - FilmWorld</title>
+        <title>{`${genreName[0].name}の映画 - FilmWorld`}</title>
       </Head>
-      <PageHeading text="映画一覧" />
+      <PageHeading primaryText={genreName[0].name} text="の映画" />
       <GenreMovies />
     </div>
   );
