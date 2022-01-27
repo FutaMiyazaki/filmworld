9fe8023ef6922b7b50299bd1edc670bf1d256125
capcap-jp <miyamiyacap@gmail.com>
2022-01-27 15:50:35 +0900

    ♻️ 背景にpaperコンポーネントを使用し、デザインを変更

--- src/components/Movies/Info/Overview.jsx
+++ src/components/Movies/Info/Overview.jsx
@@ -1,13 +1,15 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Paper, Typography } from "@mui/material";
 import { InfoHeader } from "src/components/Movies/Info/InfoHeader";
 
 export function Overview(props) {
   return (
     <Box>
       <InfoHeader text="あらすじ" />
-      <Typography variant="body2" color="white">
-        {props?.overview}
-      </Typography>
+      <Paper sx={{ p: 1 }}>
+        <Typography variant="body2" color="white">
+          {props?.overview}
+        </Typography>
+      </Paper>
     </Box>
   );
 }
