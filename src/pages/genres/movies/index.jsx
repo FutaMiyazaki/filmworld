a68c7ab4dfb85a04d16925567269cff173a5b16b
capcap-jp <miyamiyacap@gmail.com>
2022-01-27 23:59:04 +0900

    🐛 build時のundefinedエラー回避

--- src/pages/genres/movies/index.jsx
+++ src/pages/genres/movies/index.jsx
@@ -1,4 +1,4 @@
-import { useCallback } from "react";
+import { useEffect, useState } from "react";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useGenres } from "src/hooks/useGenres";
@@ -8,21 +8,22 @@ import { GenreMovies } from "src/components/Movies/GenreMovies";
 export default function Posts() {
   const router = useRouter();
   const { data, error, isLoading } = useGenres();
+  const [genre, setGenre] = useState("");
 
-  const genreSearch = useCallback(
-    data?.genres.filter((genre) => {
-      return genre.id == router.query.genre_id;
-    }),
-    []
-  );
-  const genreName = genreSearch[0].name;
+  const genreSearch = useEffect(() => {
+    for (let i = 0; i < data?.genres.length; i++) {
+      if (data?.genres[i].id == router.query.genre_id) {
+        setGenre(data?.genres[i].name);
+      }
+    }
+  });
 
   return (
     <div>
       <Head>
-        <title>{`${genreName}の映画 - FilmWorld`}</title>
+        <title>{`${genre}の映画 - FilmWorld`}</title>
       </Head>
-      <PageHeading primaryText={genreName} text="の映画" />
+      <PageHeading primaryText={genre} text="の映画" />
       <GenreMovies />
     </div>
   );
