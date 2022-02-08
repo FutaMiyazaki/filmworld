a9990b39256158ba5c09d62f18ec999f3a42399f
capcap-jp <miyamiyacap@gmail.com>
2022-02-09 04:01:08 +0900

    ⚡️ ソート機能を追加

--- src/hooks/useGenreMovies.jsx
+++ src/hooks/useGenreMovies.jsx
@@ -5,15 +5,22 @@ import useSWR from "swr";
 export const useGenreMovies = () => {
   const router = useRouter();
 
-  const { data, error } = useSWR(
-    router.query.genre_id && router.query.page
-      ? `https://api.themoviedb.org/3/discover/movie?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&language=ja-JP&page=${router.query.page}&include_adult=false&with_genres=${router.query.genre_id}`
+  const { data: movies, error: moviesError } = useSWR(
+    router.query.id && router.query.page && router.query.sort
+      ? `https://api.themoviedb.org/3/discover/movie?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&language=ja-JP&page=${router.query.page}&include_adult=false&with_genres=${router.query.id}&sort_by=${router.query.sort}`
       : null,
     fetcher
   );
+
+  const { data: genres, error: genresError } = useSWR(
+    "https://api.themoviedb.org/3/genre/movie/list?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&language=ja-JP",
+    fetcher
+  );
+
   return {
-    data,
-    error,
-    isLoading: !error && !data,
+    movies,
+    genres,
+    error: moviesError || genresError,
+    isLoading: !movies && !genres && !moviesError && !genresError,
   };
 };
