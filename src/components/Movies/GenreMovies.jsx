b29a8f7483fe302f5e84cacef034f591e7acb135
capcap-jp <miyamiyacap@gmail.com>
2022-02-09 04:02:39 +0900

    ✨ ソート機能を追加

--- src/components/Movies/GenreMovies.jsx
+++ src/components/Movies/GenreMovies.jsx
@@ -1,43 +1,31 @@
 import { useMediaQuery } from "react-responsive";
-import { Loading } from "src/components/Layout/Loading";
 import { Grid, Pagination } from "@mui/material";
 import { MobileCard } from "src/components/Movies/Card/MobileCard";
 import { PcCard } from "src/components/Movies/Card/Pccard";
-import { useGenreMovies } from "src/hooks/useGenreMovies";
 import { useState } from "react";
 import { useRouter } from "next/router";
 
-export function GenreMovies() {
-  const isMobileScreen = useMediaQuery({ query: "(max-width: 600px)" });
-  const { data, error, isLoading } = useGenreMovies();
-  const [page, setPage] = useState(1);
-
+export function GenreMovies({ movies }) {
   const router = useRouter();
+  const isMobileScreen = useMediaQuery({ query: "(max-width: 600px)" });
+  const [page, setPage] = useState(1);
 
   const handlePage = (e, clickPage) => {
     setPage((page) => clickPage);
     router.push(
-      `/genres/movies?genre_id=${router.query.genre_id}&page=${clickPage}`
+      `/genres/movies?genre_id=${router.query.genre_id}&page=${clickPage}&sort=${router.query.sort}`
     );
   };
-
-  if (isLoading) {
-    return <Loading />;
-  }
-
-  if (error) {
-    return <div>{error.message}</div>;
-  }
 
   return (
     <div>
       <Grid
         container
         spacing={2}
-        justifyContent="center"
+        justifyContent="flex-start"
         columns={{ xs: 4, sm: 8 }}
       >
-        {data.results.map((movie) => {
+        {movies?.results.map((movie) => {
           return (
             <Grid key={movie.id} item xs={4} sm={4}>
               {isMobileScreen ? (
@@ -49,15 +37,17 @@ export function GenreMovies() {
           );
         })}
       </Grid>
-      <Grid container justifyContent="center" spacing={1} sx={{ mt: 3 }}>
-        <Pagination
-          count={data.total_pages}
-          variant="outlined"
-          shape="rounded"
-          color="primary"
-          onChange={handlePage}
-        />
-      </Grid>
+      {movies?.total_pages == 1 ? null : (
+        <Grid container justifyContent="center" spacing={1} sx={{ mt: 3 }}>
+          <Pagination
+            count={movies?.total_pages}
+            variant="outlined"
+            shape="rounded"
+            color="primary"
+            onChange={handlePage}
+          />
+        </Grid>
+      )}
     </div>
   );
 }
---
+++ src/pages/movies/genre/index.jsx
@@ -0,0 +1,49 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useCallback, useEffect, useState } from "react";
+import { Loading } from "src/components/Layout/Loading";
+import { SortMenu } from "src/components/Layout/Form/SortMenu";
+import { PageHeading } from "src/components/Layout/PageHeading";
+import { GenreMovies } from "src/components/Movies/GenreMovies";
+import { useGenreMovies } from "src/hooks/useGenreMovies";
+
+export default function Posts() {
+  const router = useRouter();
+  const [genre, setGenre] = useState("");
+  const [sort, setSort] = useState("popularity.desc");
+  const { movies, genres, error, isLoading } = useGenreMovies();
+
+  const genreSearch = useEffect(() => {
+    for (let i = 0; i < genres?.genres.length; i++) {
+      if (genres?.genres[i].id == router.query.genre_id) {
+        setGenre(genres?.genres[i].name);
+      }
+    }
+  });
+
+  const handleChangeSort = useCallback(
+    (e) => {
+      const newSort = e.target.value;
+      setSort(newSort);
+      router.push(`/movies/genre?id=${router.query.id}&page=1&sort=${newSort}`);
+    },
+    [sort]
+  );
+
+  return (
+    <div>
+      <Head>
+        <title>{`${genre}の映画 - FilmWorld`}</title>
+      </Head>
+      <PageHeading primaryText={genre} text="の映画" />
+      <SortMenu sort={sort} handleChangeSort={handleChangeSort} />
+      {isLoading ? (
+        <Loading />
+      ) : error ? (
+        <div>{error.message}</div>
+      ) : (
+        <GenreMovies movies={movies} />
+      )}
+    </div>
+  );
+}
