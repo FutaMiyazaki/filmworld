e63be948a424278aa6e7db13c98ea5d4d4dca122
capcap-jp <miyamiyacap@gmail.com>
2022-02-11 05:06:19 +0900

    ♻️ 返り値の定数名を変更

--- src/hooks/useSearchMovie.jsx
+++ src/hooks/useSearchMovie.jsx
@@ -4,7 +4,7 @@ import useSWR from "swr";
 
 export const useSearchMovies = () => {
   const router = useRouter();
-  const { data, error } = useSWR(
+  const { data: movies, error } = useSWR(
     router.query.page && router.query.keyword
       ? `https://api.themoviedb.org/3/search/movie?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&page=${router.query.page}&query=${router.query.keyword}&language=ja-JP&include_adult=false`
       : null,
@@ -12,9 +12,9 @@ export const useSearchMovies = () => {
   );
 
   return {
-    data,
+    movies,
     error,
-    isLoading: !error && !data,
-    isEmpty: data && data.length === 0,
+    isLoading: !movies && !error,
+    isEmpty: movies && movies.length === 0,
   };
 };
