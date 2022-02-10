7aa7b44b45f92b0e128730bb0a64b5e95ae8d6a2
capcap-jp <miyamiyacap@gmail.com>
2022-02-11 03:01:05 +0900

    ♻️ ページネーションをコンポーネントに切り出す

--- src/components/Movies/PopularMovies.jsx
+++ src/components/Movies/PopularMovies.jsx
@@ -1,30 +1,16 @@
-import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { useMediaQuery } from "react-responsive";
-import { Grid, Pagination } from "@mui/material";
+import { Grid } from "@mui/material";
+import { AppPagination } from "src/components/Layout/AppPagination";
 import { Loading } from "src/components/Layout/Loading";
 import { MobileCard } from "src/components/Movies/Card/MobileCard";
 import { PcCard } from "src/components/Movies/Card/Pccard";
 import { usePopularMovies } from "src/hooks/usePopularMovies";
 
 export function PopularMovies() {
-  const isMobileScreen = useMediaQuery({ query: "(max-width: 600px)" });
   const router = useRouter();
-  const [page, setPage] = useState(1);
+  const isMobileScreen = useMediaQuery({ query: "(max-width: 600px)" });
   const { movies, error, isLoading } = usePopularMovies();
-
-  const handlePage = (e, clickPage) => {
-    setPage((page) => clickPage);
-    router.push(
-      router.query.year
-        ? `/movies/popular?page=${clickPage}&year=${router.query.year}`
-        : `/movies/popular?page=${clickPage}`
-    );
-  };
-
-  useEffect(() => {
-    setPage(1);
-  }, [router.query.year]);
 
   if (isLoading) {
     return <Loading />;
@@ -54,19 +40,14 @@ export function PopularMovies() {
           );
         })}
       </Grid>
-      {movies?.total_pages === 1 ? null : (
-        <Grid container justifyContent="center" spacing={1} sx={{ mt: 4 }}>
-          <Pagination
-            page={page}
-            count={movies.total_pages}
-            variant="outlined"
-            shape="rounded"
-            color="primary"
-            size={isMobileScreen ? "small" : "medium"}
-            onChange={handlePage}
-          />
-        </Grid>
-      )}
+      <AppPagination
+        movies={movies}
+        path={
+          router.query.year
+            ? `/movies/popular?year=${router.query.year}&`
+            : `/movies/popular?`
+        }
+      />
     </div>
   );
 }
