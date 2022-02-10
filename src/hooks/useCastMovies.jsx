f4416649a9afc7af89241cf5ab4615457b65f88a
capcap-jp <miyamiyacap@gmail.com>
2022-02-11 01:41:45 +0900

    🔥 出演者情報は別hookで取得するように変更

--- src/hooks/useCastMovies.jsx
+++ src/hooks/useCastMovies.jsx
@@ -5,22 +5,15 @@ import useSWR from "swr";
 export const useCastMovies = () => {
   const router = useRouter();
   const { data: movies, error: moviesError } = useSWR(
-    router.query.id && router.query.page
+    router.query.id && router.query.page && router.query.sort
       ? `https://api.themoviedb.org/3/discover/movie?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&language=ja-JP&page=${router.query.page}&include_adult=false&with_people=${router.query.id}&sort_by=${router.query.sort}&region=JP`
-      : null,
-    fetcher
-  );
-  const { data: cast, error: castError } = useSWR(
-    router.query.id
-      ? `https://api.themoviedb.org/3/person/${router.query.id}?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&language=ja-JP`
       : null,
     fetcher
   );
 
   return {
     movies,
-    cast,
-    error: moviesError || castError,
-    isLoading: !movies && !cast && !moviesError && !castError,
+    error: moviesError,
+    isLoading: !movies && !moviesError,
   };
 };
