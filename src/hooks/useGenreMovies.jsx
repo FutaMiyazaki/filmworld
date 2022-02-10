df3eeae2816df51b0a469116b1123985d3f5f1bb
capcap-jp <miyamiyacap@gmail.com>
2022-02-11 03:29:37 +0900

    🔥 ジャンル一覧取得は別hooksに切り出し

--- src/hooks/useGenreMovies.jsx
+++ src/hooks/useGenreMovies.jsx
@@ -5,22 +5,16 @@ import useSWR from "swr";
 export const useGenreMovies = () => {
   const router = useRouter();
 
-  const { data: movies, error: moviesError } = useSWR(
+  const { data: movies, error } = useSWR(
     router.query.id && router.query.page && router.query.sort
       ? `https://api.themoviedb.org/3/discover/movie?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&language=ja-JP&page=${router.query.page}&include_adult=false&with_genres=${router.query.id}&sort_by=${router.query.sort}&region=JP`
       : null,
     fetcher
   );
 
-  const { data: genres, error: genresError } = useSWR(
-    "https://api.themoviedb.org/3/genre/movie/list?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&language=ja-JP",
-    fetcher
-  );
-
   return {
     movies,
-    genres,
-    error: moviesError || genresError,
-    isLoading: !movies && !genres && !moviesError && !genresError,
+    error,
+    isLoading: !movies && !error,
   };
 };
