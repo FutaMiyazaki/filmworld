b8b5db9e83342c9da7f69f8369d2008a3404c568
capcap-jp <miyamiyacap@gmail.com>
2022-02-11 03:18:19 +0900

    ♻️ 返り値の定数名を修正

--- src/hooks/useGenres.jsx
+++ src/hooks/useGenres.jsx
@@ -2,13 +2,14 @@ import { fetcher } from "src/utils/fetcher";
 import useSWR from "swr";
 
 export const useGenres = () => {
-  const { data, error } = useSWR(
+  const { data: genres, error: genresError } = useSWR(
     "https://api.themoviedb.org/3/genre/movie/list?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&language=ja-JP",
     fetcher
   );
+
   return {
-    data,
-    error,
-    isLoading: !error && !data,
+    genres,
+    genresError,
+    isLoading: !genres && !genresError,
   };
 };
