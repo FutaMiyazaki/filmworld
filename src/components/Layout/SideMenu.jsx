f31f5dd48b7297d8d0832da7610cc68c13d71d97
capcap-jp <miyamiyacap@gmail.com>
2022-02-22 17:19:42 +0900

    💄 サイドメニューを追加

---
+++ src/components/Layout/SideMenu.jsx
@@ -0,0 +1,184 @@
+import { useState } from "react";
+import NextLink from "next/link";
+import {
+  Collapse,
+  Divider,
+  Link as MuiLink,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import VideoLibraryIcon from "@mui/icons-material/VideoLibrary";
+
+export function SideMenu() {
+  const [open, setOpen] = useState(false);
+  const rankingListItems = [
+    {
+      text: "人気の映画",
+      path: "/movies/popular?page=1",
+    },
+    {
+      text: "歴代興行収入",
+      path: "/movies/revenue?page=1",
+    },
+  ];
+  const genres1 = [
+    {
+      text: "アクション",
+      id: "28",
+    },
+    {
+      text: "アドベンチャー",
+      id: "12",
+    },
+    {
+      text: "アニメーション",
+      id: "16",
+    },
+    {
+      text: "コメディ",
+      id: "35",
+    },
+  ];
+  const genres2 = [
+    {
+      text: "ドキュメンタリー",
+      id: "99",
+    },
+    {
+      text: "ファミリー",
+      id: "10751",
+    },
+    {
+      text: "ファンタジー",
+      id: "14",
+    },
+    {
+      text: "ホラー",
+      id: "27",
+    },
+    {
+      text: "ロマンス",
+      id: "10749",
+    },
+    {
+      text: "サイエンスフィクション",
+      id: "878",
+    },
+  ];
+
+  const handleMoreOpen = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div>
+      <List sx={{ pt: 0, position: "fixed" }}>
+        <NextLink href="/search" passHref>
+          <MuiLink underline="none" color="white">
+            <ListItem disablePadding>
+              <ListItemButton sx={{ py: 0 }}>
+                <ListItemText>
+                  <Typography color="primary" sx={{ fontWeight: "bold" }}>
+                    おすすめの映画を探す
+                  </Typography>
+                </ListItemText>
+              </ListItemButton>
+            </ListItem>
+          </MuiLink>
+        </NextLink>
+        <ListItem>
+          <ListItemText>
+            <Typography color="white" sx={{ fontWeight: "bold" }}>
+              ランキングで探す
+            </Typography>
+          </ListItemText>
+        </ListItem>
+        {rankingListItems.map((listItem) => (
+          <NextLink key={listItem.text} href={listItem.path} passHref>
+            <MuiLink underline="none" color="white">
+              <ListItem dense>
+                <ListItemButton>
+                  <ListItemText>{listItem.text}</ListItemText>
+                </ListItemButton>
+              </ListItem>
+            </MuiLink>
+          </NextLink>
+        ))}
+        <Divider />
+        <ListItem>
+          <ListItemText>
+            <Typography color="white" sx={{ fontWeight: "bold" }}>
+              ジャンルで探す
+            </Typography>
+          </ListItemText>
+        </ListItem>
+        {genres1.map((genre) => (
+          <NextLink
+            key={genre.id}
+            href={`/movies/genre?id=${genre.id}&sort=popularity.desc&&page=1`}
+            passHref
+          >
+            <MuiLink underline="none" color="white">
+              <ListItem dense>
+                <ListItemButton>
+                  <ListItemText>{genre.text}</ListItemText>
+                </ListItemButton>
+              </ListItem>
+            </MuiLink>
+          </NextLink>
+        ))}
+        <ListItem dense>
+          <ListItemButton onClick={handleMoreOpen}>
+            <ListItemText>さらに表示</ListItemText>
+            {open ? <ExpandLess /> : <ExpandMore />}
+          </ListItemButton>
+        </ListItem>
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          {genres2.map((genre) => (
+            <NextLink
+              key={genre.id}
+              href={`/movies/genre?id=${genre.id}&sort=popularity.desc&&page=1`}
+              passHref
+            >
+              <MuiLink underline="none" color="white">
+                <ListItem dense>
+                  <ListItemButton>
+                    <ListItemText>{genre.text}</ListItemText>
+                  </ListItemButton>
+                </ListItem>
+              </MuiLink>
+            </NextLink>
+          ))}
+          <NextLink href="/genres" passHref>
+            <MuiLink underline="none" color="white">
+              <ListItem dense>
+                <ListItemButton>
+                  <ListItemText>ジャンル一覧を表示</ListItemText>
+                </ListItemButton>
+              </ListItem>
+            </MuiLink>
+          </NextLink>
+        </Collapse>
+        <Divider />
+        <NextLink href="/favorite" passHref>
+          <MuiLink underline="none" color="white">
+            <ListItem dense>
+              <ListItemButton>
+                <ListItemIcon>
+                  <VideoLibraryIcon />
+                </ListItemIcon>
+                <ListItemText>ウォッチリスト</ListItemText>
+              </ListItemButton>
+            </ListItem>
+          </MuiLink>
+        </NextLink>
+      </List>
+    </div>
+  );
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,7 @@
+import { Grid } from "@mui/material";
 import Head from "next/head";
 import { PageHeadingLink } from "src/components/Layout/Link/PageHeadingLink";
+import { SideMenu } from "src/components/Layout/SideMenu";
 import { PopularMovies } from "src/components/Movies/Top/PopularMovies";
 import { RevenueMovies } from "src/components/Movies/Top/RevenueMovies";
 
@@ -9,13 +11,23 @@ export default function Index() {
       <Head>
         <title>FilmWorld</title>
       </Head>
-      <PageHeadingLink path="/movies/popular?page=1" text="人気ランキング" />
-      <PopularMovies />
-      <PageHeadingLink
-        path="/movies/popular?page=1"
-        text="興行収入ランキング"
-      />
-      <RevenueMovies />
+      <Grid container spacing={1} columns={{ xs: 4, sm: 8 }}>
+        <Grid item xs={0} sm={2} sx={{ display: { xs: "none", sm: "flex" } }}>
+          <SideMenu />
+        </Grid>
+        <Grid item xs={4} sm={6}>
+          <PageHeadingLink
+            path="/movies/popular?page=1"
+            text="人気ランキング"
+          />
+          <PopularMovies />
+          <PageHeadingLink
+            path="/movies/popular?page=1"
+            text="興行収入ランキング"
+          />
+          <RevenueMovies />
+        </Grid>
+      </Grid>
     </div>
   );
 }
