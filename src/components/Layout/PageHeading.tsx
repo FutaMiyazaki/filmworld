e121e2e5fb55e5c587eb831311744711ef15e3d7
capcap-jp <miyamiyacap@gmail.com>
2022-02-24 19:39:13 +0900

    ⚡️ propsに応じてリンクを持つか切り替える・typescript化 #10

---
+++ src/components/Layout/PageHeading.tsx
@@ -0,0 +1,34 @@
+import NextLink from "next/link";
+import { Grid, Link as MuiLink, Typography } from "@mui/material";
+
+type PageHeadingProps = {
+  path?: string;
+  text: string;
+};
+
+export function PageHeading(props: PageHeadingProps) {
+  const { path, text } = props;
+
+  return (
+    <Grid container direction="row" alignItems="center" sx={{ mb: 2 }}>
+      <Grid item>
+        {path ? (
+          <NextLink href={path} passHref>
+            <MuiLink underline="none" sx={{ color: "white" }}>
+              <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+                {text}
+              </Typography>
+            </MuiLink>
+          </NextLink>
+        ) : (
+          <Typography
+            variant="h6"
+            sx={{ display: "inline", fontWeight: "bold" }}
+          >
+            {text}
+          </Typography>
+        )}
+      </Grid>
+    </Grid>
+  );
+}
