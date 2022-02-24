d03639330262fe4f4bb3239263aadf6053180092
capcap-jp <miyamiyacap@gmail.com>
2022-02-25 00:52:42 +0900

    ✨ TypeScript化およびpropsの型を定義

---
+++ src/components/Movies/Info/CastInformation.tsx
@@ -0,0 +1,52 @@
+import NextLink from "next/link";
+import { Box, Link as MuiLink, Paper, Typography } from "@mui/material";
+import { InfoHeader } from "src/components/Movies/Info/InfoHeader";
+
+type CastData = {
+  id: number;
+  name: string;
+}[];
+
+type CastProps = {
+  cast: CastData;
+};
+
+export function CastInformation(props: CastProps) {
+  const { cast } = props;
+
+  return (
+    <>
+      {cast?.length ? (
+        <Box>
+          <InfoHeader text="出演者" />
+          {cast?.map(({ id, name }, i: number) => {
+            return i < 10 ? (
+              <NextLink
+                key={id}
+                href={`/movies/cast?id=${id}&sort=popularity.desc&page=1`}
+                passHref
+              >
+                <MuiLink underline="none">
+                  <Paper
+                    sx={{
+                      display: "inline-block",
+                      textAlign: "center",
+                      px: 1,
+                      mr: 1,
+                      mb: 1,
+                      "&:hover": {
+                        opacity: 0.6,
+                      },
+                    }}
+                  >
+                    <Typography variant="body2">{name}</Typography>
+                  </Paper>
+                </MuiLink>
+              </NextLink>
+            ) : null;
+          })}
+        </Box>
+      ) : null}
+    </>
+  );
+}
