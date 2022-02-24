fe5eea689ed3e470cfbc4f09e71fa9886555b0da
capcap-jp <miyamiyacap@gmail.com>
2022-02-24 23:32:17 +0900

    ♻️ 一部のpropsをオプショナルにする

--- src/components/Movies/Card/index.tsx
+++ src/components/Movies/Card/index.tsx
@@ -14,10 +14,10 @@ type MoviesCardProps = {
   movie: {
     id: number;
     title: string;
-    posterPath: string;
-    releaseDate: string;
-    voteAverage: number;
-    voteCount: number;
+    posterPath?: string;
+    releaseDate?: string;
+    voteAverage?: number;
+    voteCount?: number;
   };
 };
 
@@ -26,56 +26,62 @@ export function MoviesCard(props: MoviesCardProps) {
   const isMobileScreen = useMediaQuery({ query: "(max-width: 600px)" });
 
   return (
-    <Card>
-      <CardContent sx={{ pa: 1 }}>
-        <Box sx={{ mb: 1 }}>
-          <NextLink href={`/movies/${movie.id}`} passHref>
-            <MuiLink underline="hover">
-              <Typography
-                variant={isMobileScreen ? "subtitle1" : "h6"}
-                sx={{ display: "inline" }}
-              >
-                {movie.title}
-              </Typography>
-            </MuiLink>
-          </NextLink>
-        </Box>
-        <Box sx={{ display: "flex" }}>
-          <Box>
-            <NextLink passHref href={`/movies/${movie.id}`}>
-              <MuiLink underline="none">
-                <CardMedia
-                  component="img"
-                  sx={{ width: isMobileScreen ? "100" : "150" }}
-                  image={`https://image.tmdb.org/t/p/w185${movie.posterPath}`}
-                  alt="ポスター画像"
+    <>
+      {movie.posterPath ? (
+        <Card>
+          <CardContent sx={{ pa: 1 }}>
+            <Box sx={{ mb: 1 }}>
+              <NextLink href={`/movies/${movie.id}`} passHref>
+                <MuiLink underline="hover">
+                  <Typography
+                    variant={isMobileScreen ? "subtitle1" : "h6"}
+                    sx={{ display: "inline" }}
+                  >
+                    {movie.title}
+                  </Typography>
+                </MuiLink>
+              </NextLink>
+            </Box>
+            <Box sx={{ display: "flex" }}>
+              <Box>
+                <NextLink passHref href={`/movies/${movie.id}`}>
+                  <MuiLink underline="none">
+                    <CardMedia
+                      component="img"
+                      sx={{ width: isMobileScreen ? "100" : "150" }}
+                      image={`https://image.tmdb.org/t/p/w185${movie.posterPath}`}
+                      alt="ポスター画像"
+                    />
+                  </MuiLink>
+                </NextLink>
+              </Box>
+              <Box sx={{ ml: 2 }}>
+                {movie.releaseDate ? (
+                  <Typography
+                    variant={isMobileScreen ? "subtitle2" : "subtitle1"}
+                    color="white"
+                    sx={{ display: "inline" }}
+                  >
+                    公開日: {movie.releaseDate?.replace(/-/g, "/")}
+                  </Typography>
+                ) : null}
+                <UserScore
+                  voteAverage={movie.voteAverage}
+                  voteCount={movie.voteCount}
+                  size={isMobileScreen ? "small" : "medium"}
                 />
-              </MuiLink>
-            </NextLink>
-          </Box>
-          <Box sx={{ ml: 2 }}>
-            <Typography
-              variant={isMobileScreen ? "subtitle2" : "subtitle1"}
-              color="white"
-              sx={{ display: "inline" }}
-            >
-              公開日: {movie.releaseDate?.replace(/-/g, "/")}
-            </Typography>
-            <UserScore
-              voteAverage={movie.voteAverage}
-              voteCount={movie.voteCount}
-              size={isMobileScreen ? "small" : "medium"}
-            />
-            <NextLink href={`/movies/${movie.id}`} passHref>
-              <MuiLink underline="hover">
-                <Typography variant="subtitle2" sx={{ display: "inline" }}>
-                  詳しい情報はこちら
-                </Typography>
-              </MuiLink>
-            </NextLink>
-          </Box>
-        </Box>
-      </CardContent>
-    </Card>
+                <NextLink href={`/movies/${movie.id}`} passHref>
+                  <MuiLink underline="hover">
+                    <Typography variant="subtitle2" sx={{ display: "inline" }}>
+                      詳しい情報はこちら
+                    </Typography>
+                  </MuiLink>
+                </NextLink>
+              </Box>
+            </Box>
+          </CardContent>
+        </Card>
+      ) : null}
+    </>
   );
 }
