dc378edf9132065d123945716b49504d50810546
capcap-jp <miyamiyacap@gmail.com>
2022-02-25 21:04:31 +0900

    ✨ TypeScript化およびpropsの型を定義

--- src/components/Movies/About/Overview.tsx
+++ src/components/Movies/About/Overview.tsx
@@ -1,13 +1,18 @@
 import { Box, Paper, Typography } from "@mui/material";
 import { InfoHeader } from "src/components/Movies/About/InfoHeader";
 
-export function Overview(props) {
+type OverviewProps = {
+  overview: string;
+};
+
+export function Overview(props: OverviewProps) {
+  const { overview } = props;
   return (
     <Box sx={{ mb: 1 }}>
       <InfoHeader text="あらすじ" />
       <Paper sx={{ p: 1 }}>
         <Typography variant="body2" color="white">
-          {props?.overview}
+          {overview}
         </Typography>
       </Paper>
     </Box>
