90ab6ec313d1098e3c29ae1852a473af99209ef1
capcap-jp <miyamiyacap@gmail.com>
2022-02-25 21:07:30 +0900

    💄 レイアウトを修正

--- src/components/Movies/About/Overview.tsx
+++ src/components/Movies/About/Overview.tsx
@@ -8,7 +8,7 @@ type OverviewProps = {
 export function Overview(props: OverviewProps) {
   const { overview } = props;
   return (
-    <Box sx={{ mb: 1 }}>
+    <Box sx={{ mb: 2 }}>
       <InfoHeader text="あらすじ" />
       <Paper sx={{ p: 1 }}>
         <Typography variant="body2" color="white">
