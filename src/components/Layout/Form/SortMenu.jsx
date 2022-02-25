9b726c6aec011edfd811838fe1231bac4182acb5
capcap-jp <miyamiyacap@gmail.com>
2022-02-25 19:32:13 +0900

    🐛 プロパティの渡し方で発生していたエラーを修正

--- src/components/Layout/Form/SortMenu.jsx
+++ src/components/Layout/Form/SortMenu.jsx
@@ -11,11 +11,11 @@ export function SortMenu({ sort, handleChangeSort }) {
     <Grid
       container
       justifyContent="flex-end"
-      xs={12}
+      xs={4}
       sm={12}
       sx={{ mt: 2, mb: 4 }}
     >
-      <Grid item xs="12" sm="3">
+      <Grid item xs={4} sm={3}>
         <FormHelperText sx={{ fontWeight: "bold" }}>並び替え</FormHelperText>
         <FormControl fullWidth>
           <Select id="select-sort" value={sort} onChange={handleChangeSort}>
