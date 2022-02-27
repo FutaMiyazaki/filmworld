5b5083bfbd547002e582aca76e5e9ba14f6d102a
capcap-jp <miyamiyacap@gmail.com>
2022-02-27 20:11:01 +0900

    ✨ 制作会社毎の映画一覧を表示するページを作成

---
+++ src/pages/movies/company/index.jsx
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useCallback, useState } from "react";
+import { CompanyMovies } from "src/components/Movies/CompanyMovies";
+import { PageHeading } from "src/components/Layout/PageHeading";
+import { SortMenu } from "src/components/Layout/Form/SortMenu";
+import { useCompany } from "src/hooks/useCompany";
+
+const MoviesCompany = () => {
+  const router = useRouter();
+  const [sort, setSort] = useState("popularity.desc");
+  const { company, companyError } = useCompany();
+
+  const handleChangeSort = useCallback(
+    (e) => {
+      const newSort = e.target.value;
+      setSort(newSort);
+      router.push(
+        `/movies/company?id=${router.query.id}&page=1&sort=${newSort}`
+      );
+    },
+    [sort]
+  );
+
+  return (
+    <div>
+      <Head>
+        <title>{company?.name}の映画 - FilmWorld</title>
+      </Head>
+      <PageHeading text={`${company?.name}の映画`} />
+      <SortMenu sort={sort} handleChangeSort={handleChangeSort} />
+      <CompanyMovies />
+    </div>
+  );
+};
+
+export default MoviesCompany;
