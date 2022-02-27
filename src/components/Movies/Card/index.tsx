4445ffb5da5a6af9886586a6883138190dc2f556
capcap-jp <miyamiyacap@gmail.com>
2022-02-27 14:35:27 +0900

    ♻️ テキストリンクを別コンポーネントを用いて表示させる

--- src/components/Movies/Card/index.tsx
+++ src/components/Movies/Card/index.tsx
@@ -9,6 +9,7 @@ import {
   Typography,
 } from "@mui/material";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { TextLink } from "src/components/Layout/Link/TextLink";
 import { useMediaQuery } from "react-responsive";
 import { UserScore } from "src/components/Movies/About/UserScore";
 
@@ -23,76 +24,67 @@ type MoviesCardProps = {
   };
 };
 
-export function MoviesCard(props: MoviesCardProps) {
+export const MoviesCard = (props: MoviesCardProps) => {
   const { movie } = props;
   const isMobileScreen = useMediaQuery({ query: "(max-width: 600px)" });
 
   return (
-    <>
-      {movie.posterPath ? (
-        <Card>
-          <CardContent sx={{ pa: 1 }}>
-            <Box sx={{ mb: 1 }}>
-              <NextLink href={`/movies/${movie.id}`} passHref>
-                <MuiLink underline="hover">
-                  <Typography
-                    variant={isMobileScreen ? "subtitle1" : "h6"}
-                    sx={{ display: "inline" }}
-                  >
-                    {movie.title}
-                  </Typography>
-                </MuiLink>
-              </NextLink>
-            </Box>
-            <Box sx={{ display: "flex" }}>
-              <Box>
-                <NextLink passHref href={`/movies/${movie.id}`}>
-                  <MuiLink underline="none">
-                    <CardMedia
-                      component="img"
-                      sx={{
-                        width: isMobileScreen ? "100" : "150",
-                        "&:hover": {
-                          opacity: 0.7,
-                        },
-                      }}
-                      image={`https://image.tmdb.org/t/p/w185${movie.posterPath}`}
-                      alt="ポスター画像"
-                    />
-                  </MuiLink>
-                </NextLink>
-              </Box>
-              <Box sx={{ ml: 2 }}>
-                {movie.releaseDate ? (
-                  <Typography
-                    variant={isMobileScreen ? "subtitle2" : "subtitle1"}
-                    color="white"
-                    sx={{ display: "inline" }}
-                  >
-                    公開日: {movie.releaseDate?.replace(/-/g, "/")}
-                  </Typography>
-                ) : null}
-                <UserScore
-                  voteAverage={movie?.voteAverage}
-                  voteCount={movie?.voteCount}
-                  size={isMobileScreen ? "small" : "medium"}
+    <Card>
+      <CardContent sx={{ pa: 1 }}>
+        <Box sx={{ mb: 1 }}>
+          <TextLink
+            path={`/movies/${movie.id}`}
+            variant={isMobileScreen ? "subtitle1" : "h6"}
+            text={movie.title}
+          />
+        </Box>
+        <Box sx={{ display: "flex" }}>
+          <Box>
+            <NextLink passHref href={`/movies/${movie.id}`}>
+              <MuiLink underline="none">
+                <CardMedia
+                  component="img"
+                  sx={{
+                    width: isMobileScreen ? "100" : "150",
+                    "&:hover": {
+                      opacity: 0.7,
+                    },
+                  }}
+                  image={`https://image.tmdb.org/t/p/w185${movie.posterPath}`}
+                  alt="ポスター画像"
                 />
-                <NextLink href={`/movies/${movie.id}`} passHref>
-                  <MuiLink underline="none">
-                    <Button
-                      fullWidth={isMobileScreen}
-                      variant="outlined"
-                      endIcon={<ChevronRightIcon />}
-                    >
-                      詳細を見る
-                    </Button>
-                  </MuiLink>
-                </NextLink>
-              </Box>
-            </Box>
-          </CardContent>
-        </Card>
-      ) : null}
-    </>
+              </MuiLink>
+            </NextLink>
+          </Box>
+          <Box sx={{ ml: 2 }}>
+            {movie.releaseDate ? (
+              <Typography
+                variant={isMobileScreen ? "subtitle2" : "subtitle1"}
+                color="white"
+                sx={{ display: "inline" }}
+              >
+                公開日: {movie.releaseDate?.replace(/-/g, "/")}
+              </Typography>
+            ) : null}
+            <UserScore
+              voteAverage={movie?.voteAverage}
+              voteCount={movie?.voteCount}
+              size={isMobileScreen ? "small" : "medium"}
+            />
+            <NextLink href={`/movies/${movie.id}`} passHref>
+              <MuiLink underline="none">
+                <Button
+                  fullWidth={isMobileScreen}
+                  variant="outlined"
+                  endIcon={<ChevronRightIcon />}
+                >
+                  詳細を見る
+                </Button>
+              </MuiLink>
+            </NextLink>
+          </Box>
+        </Box>
+      </CardContent>
+    </Card>
   );
-}
+};
