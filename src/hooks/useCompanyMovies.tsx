0b5cc0503ff9971000cebbce2bc1de2790a984d5
capcap-jp <miyamiyacap@gmail.com>
2022-02-27 20:05:44 +0900

    ✨ id指定の制作会社が作成した映画一覧をAPIから取得するhooksを作成

---
+++ src/hooks/useCompanyMovies.tsx
@@ -0,0 +1,19 @@
+import { useRouter } from "next/router";
+import { fetcher } from "src/utils/fetcher";
+import useSWR from "swr";
+
+export const useCompanyMovies = () => {
+  const router = useRouter();
+  const { data: movies, error } = useSWR(
+    router.query.id && router.query.page && router.query.sort
+      ? `https://api.themoviedb.org/3/discover/movie?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&language=ja-JP&page=${router.query.page}&include_adult=false&with_companies=${router.query.id}&sort_by=${router.query.sort}&region=JP`
+      : null,
+    fetcher
+  );
+
+  return {
+    movies,
+    error,
+    isLoading: !movies && !error,
+  };
+};
