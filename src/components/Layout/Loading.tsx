44fe204e0a44b9474f6c7adf7ccbafd3adfc8f8b
capcap-jp <miyamiyacap@gmail.com>
2022-03-03 02:21:42 +0900

    ✨ TypeScript化

--- src/components/Layout/Loading.tsx
+++ src/components/Layout/Loading.tsx
@@ -3,7 +3,7 @@ import { Box, CircularProgress, Grid } from "@mui/material";
 export const Loading = () => {
   return (
     <Box>
-      <Grid container justifyContent="center" alignItems="center">
+      <Grid container alignItems="center" justifyContent="center">
         <CircularProgress size={80} sx={{ mt: 20 }} />
       </Grid>
     </Box>
