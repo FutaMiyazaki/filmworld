02bd0e71f98f747f881edfef27f650ba5f7f1cd0
capcap-jp <miyamiyacap@gmail.com>
2022-03-02 20:46:43 +0900

    🔥 Gridは親コンポーネントで記述するように変更

--- src/components/Layout/Form/SortMenu.jsx
+++ src/components/Layout/Form/SortMenu.jsx
@@ -1,15 +1,8 @@
-import {
-  FormControl,
-  FormHelperText,
-  Grid,
-  MenuItem,
-  Select,
-} from "@mui/material";
+import { FormControl, FormHelperText, MenuItem, Select } from "@mui/material";
 
 export const SortMenu = ({ sort, handleChangeSort }) => {
   return (
-    <Grid container columns={{ xs: 4, sm: 12 }} sx={{ mt: 2, mb: 4 }}>
-      <Grid item xs={4} sm={3}>
+    <>
         <FormHelperText sx={{ fontWeight: "bold" }}>並び替え</FormHelperText>
         <FormControl fullWidth>
           <Select
@@ -23,7 +16,6 @@ export const SortMenu = ({ sort, handleChangeSort }) => {
             <MenuItem value="revenue.desc">興行収入が多い順</MenuItem>
           </Select>
         </FormControl>
-      </Grid>
-    </Grid>
+    </>
   );
 };
