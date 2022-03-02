6d23905e6ea347d6160bfe97f9e2824f7697285f
capcap-jp <miyamiyacap@gmail.com>
2022-03-02 16:25:43 +0900

    ✨ 評価数ランキングページへのリンクを追加

--- src/components/Layout/TemporaryDrawer.jsx
+++ src/components/Layout/TemporaryDrawer.jsx
@@ -35,6 +35,10 @@ export const TemporaryDrawer = () => {
       text: "歴代興行収入",
       path: "/movies/revenue?page=1",
     },
+    {
+      text: "評価数",
+      path: "/movies/score?page=1",
+    },
   ];
   const genres1 = [
     {
