5c092b415d404572f58c6556c46a619038c234db
capcap-jp <miyamiyacap@gmail.com>
2022-03-02 21:53:16 +0900

    ✨ 特定の公開年ごとの映画をfetchできるように変更

--- src/hooks/useCastMovies.tsx
+++ src/hooks/useCastMovies.tsx
@@ -6,7 +6,9 @@ export const useCastMovies = () => {
   const router = useRouter();
   const { data: movies, error } = useSWR(
     router.query.id && router.query.page && router.query.sort
-      ? `https://api.themoviedb.org/3/discover/movie?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&language=ja-JP&page=${router.query.page}&include_adult=false&with_people=${router.query.id}&sort_by=${router.query.sort}&region=JP`
+      ? router.query.year
+        ? `https://api.themoviedb.org/3/discover/movie?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&language=ja-JP&page=${router.query.page}&include_adult=false&with_people=${router.query.id}&sort_by=${router.query.sort}&year=${router.query.year}&region=JP`
+        : `https://api.themoviedb.org/3/discover/movie?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&language=ja-JP&page=${router.query.page}&include_adult=false&with_people=${router.query.id}&sort_by=${router.query.sort}&region=JP`
       : null,
     fetcher
   );
