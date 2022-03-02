fc1b0220e6b064b552dc77a880ea4e06396a7bfb
capcap-jp <miyamiyacap@gmail.com>
2022-03-02 20:33:39 +0900

    🔥 Gridコンポーネントは親コンポーネントで記載するように変更

--- src/components/Layout/Form/FilterByYear.jsx
+++ src/components/Layout/Form/FilterByYear.jsx
@@ -1,6 +1,6 @@
 import { useCallback, useState } from "react";
 import { useRouter } from "next/router";
-import { FormHelperText, Grid, InputAdornment, TextField } from "@mui/material";
+import { FormHelperText, InputAdornment, TextField } from "@mui/material";
 
 export const FilterByYear = ({ path }) => {
   const router = useRouter();
@@ -19,30 +19,26 @@ export const FilterByYear = ({ path }) => {
   });
 
   return (
-    <Grid container columns={{ xs: 4, sm: 8 }} sx={{ mt: 2, mb: 4 }}>
-      <Grid item xs={4} sm={2}>
-        <FormHelperText sx={{ fontWeight: "bold" }}>
-          年代で絞り込む(1950~2022年)
-        </FormHelperText>
-        <form action="" onSubmit={handleSubmit}>
-          <TextField
-            fullWidth
-            size="small"
-            id="filter-by-year"
-            type="number"
-            value={year}
-            onChange={handleChange}
-            error={year && (year < 1950 || year > 2022) ? true : false}
-            variant="outlined"
-            placeholder="年代を入力してください"
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="start">年</InputAdornment>
-              ),
-            }}
-          />
-        </form>
-      </Grid>
-    </Grid>
+    <>
+      <FormHelperText sx={{ fontWeight: "bold" }}>
+        年代で絞り込む(1950~2022年)
+      </FormHelperText>
+      <form action="" onSubmit={handleSubmit}>
+        <TextField
+          fullWidth
+          size="small"
+          id="filter-by-year"
+          type="number"
+          value={year}
+          onChange={handleChange}
+          error={year && (year < 1950 || year > 2022) ? true : false}
+          variant="outlined"
+          placeholder="年代を入力してください"
+          InputProps={{
+            endAdornment: <InputAdornment position="start">年</InputAdornment>,
+          }}
+        />
+      </form>
+    </>
   );
 };
