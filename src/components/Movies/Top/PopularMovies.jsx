0d251c010198dde0ae509cc3c08447fc1575dac9
capcap-jp <miyamiyacap@gmail.com>
2022-03-06 19:20:25 +0900

    ♻️ 映画一覧の表示に関して、共通化されたコンポーネントを利用するように変更

--- src/components/Movies/Top/PopularMovies.jsx
+++ src/components/Movies/Top/PopularMovies.jsx
@@ -1,51 +1,34 @@
 import { Grid } from "@mui/material";
 import { ButtonLink } from "src/components/Layout/Link/ButtonLink";
-import { Loading } from "src/components/Layout/Loading";
-import { MoviesCard } from "src/components/Movies/Card/index";
+import { Movies } from "src/components/Movies/index";
 import { usePopularMovies } from "src/hooks/topMovies/usePopularMovies";
 
 export const PopularMovies = () => {
-  const { popularMovies, error, isLoading } = usePopularMovies();
-
-  if (isLoading) {
-    return <Loading />;
-  }
-
-  if (error) {
-    return <div>{error.message}</div>;
-  }
+  const { movies, error, isLoading } = usePopularMovies();
 
   return (
-    <Grid
-      container
-      spacing={2}
-      justifyContent="center"
-      columns={{ xs: 4, sm: 8 }}
-      sx={{ mb: 5 }}
-    >
-      {popularMovies.results.map((movie, i) => {
-        return i < 8 ? (
-          <Grid key={movie.id} item xs={4} sm={4}>
-            <MoviesCard
-              movie={{
-                id: movie.id,
-                title: movie.title,
-                posterPath: movie.poster_path,
-                releaseDate: movie.release_date,
-                voteAverage: movie.vote_average,
-                voteCount: movie.vote_count,
-              }}
-            />
-          </Grid>
-        ) : null;
-      })}
-      <Grid item xs={4} sm={4} sx={{ mt: 1 }}>
-        <ButtonLink
-          path="/movies/popular?page=1"
-          text="もっと見る"
-          variant="outlined"
-        />
+    <>
+      <Movies
+        movies={movies?.results}
+        error={error}
+        isLoading={isLoading}
+        path="/"
+        totalPages={1}
+      />
+      <Grid
+        container
+        justifyContent="center"
+        columns={{ xs: 4, sm: 8 }}
+        sx={{ my: 4 }}
+      >
+        <Grid item xs={4} sm={4}>
+          <ButtonLink
+            path="/movies/popular?page=1"
+            text="もっと見る"
+            variant="outlined"
+          />
+        </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 };
--- src/components/Movies/Top/RevenueMovies.jsx
+++ src/components/Movies/Top/RevenueMovies.jsx
@@ -1,51 +1,34 @@
 import { Grid } from "@mui/material";
 import { ButtonLink } from "src/components/Layout/Link/ButtonLink";
-import { Loading } from "src/components/Layout/Loading";
-import { MoviesCard } from "src/components/Movies/Card/index";
+import { Movies } from "src/components/Movies/index";
 import { useRevenueMovies } from "src/hooks/topMovies/useRevenueMovies";
 
 export const RevenueMovies = () => {
-  const { revenueMovies, error, isLoading } = useRevenueMovies();
-
-  if (isLoading) {
-    return <Loading />;
-  }
-
-  if (error) {
-    return <div>{error.message}</div>;
-  }
+  const { movies, error, isLoading } = useRevenueMovies();
 
   return (
-    <Grid
-      container
-      spacing={2}
-      justifyContent="center"
-      columns={{ xs: 4, sm: 8 }}
-      sx={{ mb: 4 }}
-    >
-      {revenueMovies.results.map((movie, i) => {
-        return i < 8 ? (
-          <Grid key={movie.id} item xs={4} sm={4}>
-            <MoviesCard
-              movie={{
-                id: movie.id,
-                title: movie.title,
-                posterPath: movie.poster_path,
-                releaseDate: movie.release_date,
-                voteAverage: movie.vote_average,
-                voteCount: movie.vote_count,
-              }}
-            />
-          </Grid>
-        ) : null;
-      })}
-      <Grid item xs={4} sm={4} sx={{ mt: 1 }}>
-        <ButtonLink
-          path="/movies/revenue?page=1"
-          text="もっと見る"
-          variant="outlined"
-        />
+    <>
+      <Movies
+        movies={movies?.results}
+        error={error}
+        isLoading={isLoading}
+        path="/"
+        totalPages={1}
+      />
+      <Grid
+        container
+        justifyContent="center"
+        columns={{ xs: 4, sm: 8 }}
+        sx={{ my: 4 }}
+      >
+        <Grid item xs={4} sm={4}>
+          <ButtonLink
+            path="/movies/revenue?page=1"
+            text="もっと見る"
+            variant="outlined"
+          />
+        </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 };
