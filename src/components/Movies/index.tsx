6d2c36e62695762b4d85122a8fa41635329b40d0
capcap-jp <miyamiyacap@gmail.com>
2022-03-06 17:36:30 +0900

    ✨ 映画一覧およびページネーションの表示をコンポーネント化

---
+++ src/components/Movies/index.tsx
@@ -0,0 +1,71 @@
+import { VFC } from "react";
+import { Grid } from "@mui/material";
+import { AppPagination } from "src/components/Layout/AppPagination";
+import { Loading } from "src/components/Layout/Loading";
+import { MoviesCard } from "src/components/Movies/Card/index";
+
+type movieType = {
+  id: number;
+  poster_path?: string;
+  release_date?: string;
+  title: string;
+  vote_average: 0;
+  vote_count: 0;
+};
+
+type moviesData = {
+  id: number;
+  poster_path?: string;
+  release_date?: string;
+  title: string;
+  vote_average: 0;
+  vote_count: 0;
+}[];
+
+type errorData = {
+  message?: string;
+};
+
+type MoviesProps = {
+  movies: moviesData;
+  error: errorData;
+  isLoading: boolean;
+  path: string;
+  totalPages: number;
+};
+
+export const Movies: VFC<MoviesProps> = (props) => {
+  const { movies, error, isLoading, path, totalPages } = props;
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <>
+      <Grid container columns={{ xs: 4, sm: 8 }} spacing={2}>
+        {movies.map((movie: movieType) => {
+          return (
+            <Grid key={movie.id} item xs={4} sm={4}>
+              <MoviesCard
+                movie={{
+                  id: movie.id,
+                  title: movie.title,
+                  posterPath: movie.poster_path,
+                  releaseDate: movie.release_date,
+                  voteAverage: movie.vote_average,
+                  voteCount: movie.vote_count,
+                }}
+              />
+            </Grid>
+          );
+        })}
+      </Grid>
+      <AppPagination path={path} totalPages={totalPages} />
+    </>
+  );
+};
