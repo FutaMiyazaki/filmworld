89f55c530a13a18b5ee01338d13e2d44471e15d7
capcap-jp <miyamiyacap@gmail.com>
2022-03-06 18:22:26 +0900

    ♻️ 映画一覧の表示に関して、共通化されたコンポーネントを利用するように変更

--- src/components/Movies/About/SimilarMovies.tsx
+++ src/components/Movies/About/SimilarMovies.tsx
@@ -1,51 +1,16 @@
-import { Grid } from "@mui/material";
 import { useSimilarMovies } from "src/hooks/useSimilarMovies";
-import { Loading } from "src/components/Layout/Loading";
-import { MoviesCard } from "src/components/Movies/Card/index";
-
-type movieType = {
-  id: number;
-  poster_path?: string;
-  release_date?: string;
-  title: string;
-  vote_average: 0;
-  vote_count: 0;
-};
+import { Movies } from "src/components/Movies/index";
 
 export const SimilarMovies = () => {
   const { similarMovies, error, isLoading } = useSimilarMovies();
 
-  if (isLoading) {
-    return <Loading />;
-  }
-
-  if (error) {
-    return <div>{error.message}</div>;
-  }
-
   return (
-    <Grid
-      container
-      columns={{ xs: 4, sm: 8 }}
-      justifyContent="center"
-      spacing={2}
-    >
-      {similarMovies.results?.map((movie: movieType, i: number) => {
-        return i < 10 ? (
-          <Grid key={movie.id} item xs={4} sm={4}>
-            <MoviesCard
-              movie={{
-                id: movie.id,
-                title: movie.title,
-                posterPath: movie.poster_path,
-                releaseDate: movie.release_date,
-                voteAverage: movie.vote_average,
-                voteCount: movie.vote_count,
-              }}
-            />
-          </Grid>
-        ) : null;
-      })}
-    </Grid>
+    <Movies
+      movies={similarMovies?.results}
+      error={error}
+      isLoading={isLoading}
+      path="/"
+      totalPages={1}
+    />
   );
 };
