13511756f519f9c0a168142098e584d5f7b7a003
capcap-jp <miyamiyacap@gmail.com>
2022-03-06 15:56:45 +0900

    ✨ TypeScript化および型を定義

--- src/components/Movies/About/SimilarMovies.tsx
+++ src/components/Movies/About/SimilarMovies.tsx
@@ -3,15 +3,24 @@ import { useSimilarMovies } from "src/hooks/useSimilarMovies";
 import { Loading } from "src/components/Layout/Loading";
 import { MoviesCard } from "src/components/Movies/Card/index";
 
+type movieType = {
+  id: number;
+  poster_path?: string;
+  release_date?: string;
+  title: string;
+  vote_average: 0;
+  vote_count: 0;
+};
+
 export const SimilarMovies = () => {
-  const { similarMovies, similarMoviesError, isLoading } = useSimilarMovies();
+  const { similarMovies, error, isLoading } = useSimilarMovies();
 
   if (isLoading) {
     return <Loading />;
   }
 
-  if (similarMoviesError) {
-    return <div>{similarMoviesError.message}</div>;
+  if (error) {
+    return <div>{error.message}</div>;
   }
 
   return (
@@ -21,7 +30,7 @@ export const SimilarMovies = () => {
       justifyContent="center"
       spacing={2}
     >
-      {similarMovies.results?.map((movie, i) => {
+      {similarMovies.results?.map((movie: movieType, i: number) => {
         return i < 10 ? (
           <Grid key={movie.id} item xs={4} sm={4}>
             <MoviesCard
