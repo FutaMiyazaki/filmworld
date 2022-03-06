ad0a3b94ff68f824be2a3f60078cb640591bf3bb
capcap-jp <miyamiyacap@gmail.com>
2022-03-06 18:35:31 +0900

    ♻️ 映画一覧とページネーションの表示に関して、共通化されたコンポーネントを利用するように変更

--- src/pages/search/movies/index.tsx
+++ src/pages/search/movies/index.tsx
@@ -3,12 +3,16 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useGenres } from "src/hooks/useGenres";
-import { SearchResultsMovies } from "src/components/Movies/SearchResultsMovies";
+import { Movies } from "src/components/Movies/index";
+import { useSearchMovies } from "src/hooks/useSearchMovie";
+import { PageHeading } from "src/components/Layout/PageHeading";
 
 const SearchMovies: NextPage = () => {
   const router = useRouter();
   const [genre, setGenre] = useState("");
+  const [path, setPath] = useState("");
   const { genres } = useGenres();
+  const { movies, error, isLoading, isEmpty } = useSearchMovies();
 
   const genreSet = useEffect(() => {
     for (let i = 0; i < genres?.genres.length; i++) {
@@ -17,6 +21,14 @@ const SearchMovies: NextPage = () => {
       }
     }
   });
+
+  useEffect(() => {
+    router.query.keyword
+      ? setPath(`/search/movies?keyword=${router.query.keyword}&`)
+      : setPath(
+          `/search/movies?genre=${router.query.genre}&year_start=${router.query.year_start}&year_end=${router.query.year_end}&`
+        );
+  }, [router]);
 
   return (
     <div>
@@ -27,7 +39,26 @@ const SearchMovies: NextPage = () => {
             : `${router.query.year_start}~${router.query.year_end}年に公開された${genre}のおすすめ映画 - FilmWorld`}
         </title>
       </Head>
-      <SearchResultsMovies />
+      {router.query.keyword ? (
+        <PageHeading
+          text={`${router.query.keyword}の検索結果 ${movies?.total_results}件`}
+        />
+      ) : (
+        <PageHeading
+          text={`${router.query.year_start}~${router.query.year_end}年に公開された${genre}のオススメ映画`}
+        />
+      )}
+      {isEmpty ? (
+        <div>見つかりませんでした</div>
+      ) : (
+        <Movies
+          movies={movies?.results}
+          error={error}
+          isLoading={isLoading}
+          path={path}
+          totalPages={movies?.total_pages}
+        />
+      )}
     </div>
   );
 };
