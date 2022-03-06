a3deefe449fa044581736664277653afc0653322
capcap-jp <miyamiyacap@gmail.com>
2022-03-06 18:12:05 +0900

    ♻️ 映画一覧とページネーションの表示に関して、共通化されたコンポーネントを利用するように変更

--- src/pages/movies/popular/index.tsx
+++ src/pages/movies/popular/index.tsx
@@ -1,14 +1,24 @@
 import { NextPage } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { Grid } from "@mui/material";
 import { FilterByYear } from "src/components/Layout/Form/FilterByYear";
 import { RankingButtonLinks } from "src/components/Layout/Link/RankingButtonLinks";
 import { PageHeading } from "src/components/Layout/PageHeading";
-import { PopularMovies } from "src/components/Movies/PopularMovies";
+import { Movies } from "src/components/Movies/index";
+import { usePopularMovies } from "src/hooks/usePopularMovies";
 
 const MoviesPopular: NextPage = () => {
   const router = useRouter();
+  const [path, setPath] = useState("");
+  const { movies, error, isLoading } = usePopularMovies();
+
+  useEffect(() => {
+    router.query.year
+      ? setPath(`/movies/popular?year=${router.query.year}&`)
+      : setPath(`/movies/popular?`);
+  }, [router]);
 
   return (
     <div>
@@ -38,7 +48,13 @@ const MoviesPopular: NextPage = () => {
           <FilterByYear path="/movies/popular?" />
         </Grid>
       </Grid>
-      <PopularMovies />
+      <Movies
+        movies={movies?.results}
+        error={error}
+        isLoading={isLoading}
+        path={path}
+        totalPages={movies?.total_pages}
+      />
     </div>
   );
 };
