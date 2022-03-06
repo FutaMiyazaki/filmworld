6589fa6bb0618c7bbb63506f011d38710d617591
capcap-jp <miyamiyacap@gmail.com>
2022-03-06 19:17:59 +0900

    ♻️ リファクタリング

--- src/hooks/topMovies/usePopularMovies.tsx
+++ src/hooks/topMovies/usePopularMovies.tsx
@@ -1,15 +1,15 @@
-import { fetcher } from "src/utils/fetcher";
 import useSWR from "swr";
+import { fetcher } from "src/utils/fetcher";
 
 export const usePopularMovies = () => {
-  const { data: popularMovies, error } = useSWR(
+  const { data: movies, error } = useSWR(
     "https://api.themoviedb.org/3/movie/popular?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&language=ja-JP&page=1&region=JP",
     fetcher
   );
 
   return {
-    popularMovies,
+    movies,
     error,
-    isLoading: !popularMovies && !error,
+    isLoading: !movies && !error,
   };
 };
--- src/hooks/topMovies/useRevenueMovies.tsx
+++ src/hooks/topMovies/useRevenueMovies.tsx
@@ -1,14 +1,14 @@
-import { fetcher } from "src/utils/fetcher";
 import useSWR from "swr";
+import { fetcher } from "src/utils/fetcher";
 
 export const useRevenueMovies = () => {
-  const { data: revenueMovies, error } = useSWR(
+  const { data: movies, error } = useSWR(
     "https://api.themoviedb.org/3/discover/movie?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&language=ja-JP&page=1&include_adult=false&sort_by=revenue.desc&region=JP",
     fetcher
   );
   return {
-    revenueMovies,
+    movies,
     error,
-    isLoading: !revenueMovies && !error,
+    isLoading: !movies && !error,
   };
 };
