f0b302b55ba7ba36f725cd05c543549c8909f8b3
capcap-jp <miyamiyacap@gmail.com>
2022-03-09 00:16:20 +0900

    ✨ TypeScript化および型を定義

--- src/components/Layout/Form/HeaderTextField.tsx
+++ src/components/Layout/Form/HeaderTextField.tsx
@@ -1,9 +1,9 @@
 import { useRouter } from "next/router";
-import { useCallback, useState } from "react";
+import { useCallback, useState, VFC } from "react";
 import { FormControl, InputAdornment, TextField } from "@mui/material/";
 import SearchIcon from "@mui/icons-material/Search";
 
-export const HeaderTextField = () => {
+export const HeaderTextField: VFC = () => {
   const router = useRouter();
   const [keyword, setKeyword] = useState("");
 
@@ -11,25 +11,28 @@ export const HeaderTextField = () => {
     setKeyword(e.target.value);
   }, []);
 
-  const handleSubmit = useCallback((e) => {
-    e.preventDefault();
-    if (keyword.length === 0) {
-      return;
-    }
-    router.push(`/search/movies?keyword=${keyword}&page=1`);
-  });
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (keyword.length < 30) {
+        router.push(`/search/movies?keyword=${keyword}&page=1`);
+      }
+    },
+    [keyword]
+  );
 
   return (
     <form action="" onSubmit={handleSubmit}>
       <FormControl sx={{ width: "25vw" }}>
         <TextField
+          autoFocus
           id="header-form"
-          type="text"
-          value={keyword}
           onChange={handleSearch}
-          size="small"
           placeholder="キーワードを入力"
+          size="small"
+          type="text"
           variant="outlined"
+          value={keyword}
           InputProps={{
             startAdornment: (
               <InputAdornment position="start">
