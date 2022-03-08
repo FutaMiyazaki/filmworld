26ad8ef8388e3ac0b54acbd197ab2582fda2c09e
capcap-jp <miyamiyacap@gmail.com>
2022-03-08 22:48:41 +0900

    ✨ モバイル画面のヘッダーに配置するボタン及び検索フォームを作成

---
+++ src/components/Layout/Header/SearchButton.tsx
@@ -0,0 +1,87 @@
+import { useRouter } from "next/router";
+import { useState, useCallback, VFC } from "react";
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  InputAdornment,
+  TextField,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+
+export const SearchButton: VFC = () => {
+  const router = useRouter();
+  const [keyword, setKeyword] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const openDialog = useCallback(() => {
+    setOpen(true);
+  }, []);
+
+  const closeDialog = useCallback(() => {
+    setOpen(false);
+  }, []);
+
+  const handleSearch = useCallback((e) => {
+    setKeyword(e.target.value);
+  }, []);
+
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (keyword.length === 0) {
+        return;
+      }
+      setOpen(false);
+      router.push(`/search/movies?keyword=${keyword}&page=1`);
+    },
+    [keyword]
+  );
+
+  return (
+    <>
+      <IconButton
+        color="primary"
+        onClick={openDialog}
+        sx={{ ml: "auto", display: "block" }}
+      >
+        <SearchIcon />
+      </IconButton>
+      <Dialog fullScreen open={open} onClose={closeDialog}>
+        <DialogTitle>
+          <Button
+            onClick={closeDialog}
+            variant="text"
+            sx={{ ml: "auto", display: "block" }}
+          >
+            キャンセル
+          </Button>
+        </DialogTitle>
+        <DialogContent>
+          <form action="" onSubmit={handleSubmit}>
+            <TextField
+              autoFocus
+              fullWidth
+              id="mobile-header-form"
+              onChange={handleSearch}
+              placeholder="キーワードを入力"
+              size="small"
+              type="text"
+              value={keyword}
+              variant="standard"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </form>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
