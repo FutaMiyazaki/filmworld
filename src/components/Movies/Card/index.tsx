5685499927dc7c0927e033a9f4b928dbc02e4ff0
capcap-jp <miyamiyacap@gmail.com>
2022-03-10 23:26:17 +0900

    💄 リンクをカード全体に適用させる

--- src/components/Movies/Card/index.tsx
+++ src/components/Movies/Card/index.tsx
@@ -1,16 +1,14 @@
 import NextLink from "next/link";
 import { VFC } from "react";
 import {
-  Button,
   Card,
+  CardActionArea,
   CardContent,
   CardMedia,
   Grid,
   Link as MuiLink,
   Typography,
 } from "@mui/material";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import { TextLink } from "src/components/Layout/Link/TextLink";
 import { useMediaQuery } from "react-responsive";
 import { UserScore } from "src/components/Movies/About/UserScore";
 
@@ -31,59 +29,44 @@ export const MoviesCard: VFC<MoviesCardProps> = (props) => {
 
   return (
     <Card>
-      <CardContent>
-        <Grid container columns={{ xs: 5 }} spacing={2}>
-          <Grid item xs={2}>
-            <NextLink passHref href={`/movies/${movie.id}`}>
-              <MuiLink underline="none">
-                <CardMedia
-                  alt="ポスター画像"
-                  component="img"
-                  image={`https://image.tmdb.org/t/p/original${movie.posterPath}`}
-                  sx={{
-                    "&:hover": {
-                      opacity: 0.7,
-                    },
-                  }}
-                />
-              </MuiLink>
-            </NextLink>
-          </Grid>
-          <Grid item xs={3}>
-            <TextLink
-              path={`/movies/${movie.id}`}
-              text={movie.title}
-              variant={isMobileScreen ? "subtitle2" : "subtitle1"}
-            />
-            {movie.releaseDate ? (
-              <Typography
-                color="white"
-                variant={isMobileScreen ? "subtitle2" : "subtitle1"}
-                sx={{ display: "inline" }}
-              >
-                公開日： {movie.releaseDate?.replace(/-/g, "/")}
-              </Typography>
-            ) : null}
-            <UserScore
-              size={isMobileScreen ? "small" : "medium"}
-              voteAverage={movie?.voteAverage}
-              voteCount={movie?.voteCount}
-            />
-            <NextLink href={`/movies/${movie.id}`} passHref>
-              <MuiLink underline="none">
-                <Button
-                  endIcon={<ChevronRightIcon />}
-                  fullWidth
-                  size="small"
-                  variant="outlined"
-                >
-                  詳細を見る
-                </Button>
-              </MuiLink>
-            </NextLink>
-          </Grid>
-        </Grid>
-      </CardContent>
+      <CardActionArea>
+        <NextLink passHref href={`/movies/${movie.id}`}>
+          <MuiLink underline="none">
+            <CardContent>
+              <Grid container columns={{ xs: 5 }} spacing={2}>
+                <Grid item xs={2}>
+                  <CardMedia
+                    alt="ポスター画像"
+                    component="img"
+                    image={`https://image.tmdb.org/t/p/original${movie.posterPath}`}
+                  />
+                </Grid>
+                <Grid item xs={3}>
+                  <Typography
+                    variant={isMobileScreen ? "subtitle2" : "subtitle1"}
+                  >
+                    {movie.title}
+                  </Typography>
+                  {movie.releaseDate ? (
+                    <Typography
+                      color="white"
+                      variant={isMobileScreen ? "subtitle2" : "subtitle1"}
+                      sx={{ display: "inline" }}
+                    >
+                      公開日： {movie.releaseDate?.replace(/-/g, "/")}
+                    </Typography>
+                  ) : null}
+                  <UserScore
+                    size={isMobileScreen ? "small" : "medium"}
+                    voteAverage={movie?.voteAverage}
+                    voteCount={movie?.voteCount}
+                  />
+                </Grid>
+              </Grid>
+            </CardContent>
+          </MuiLink>
+        </NextLink>
+      </CardActionArea>
     </Card>
   );
 };
