ce43dcc566f84d0d4df373ce42cb1b6883d1b331
capcap-jp <miyamiyacap@gmail.com>
2022-03-13 22:04:21 +0900

    💄 サイドメニューを追加

--- src/pages/genres/index.tsx
+++ src/pages/genres/index.tsx
@@ -1,7 +1,9 @@
 import { NextPage } from "next";
 import Head from "next/head";
-import { PageHeading } from "src/components/Layout/PageHeading";
+import { Grid } from "@mui/material";
 import { GenreList } from "src/components/Genre/GenreList";
+import { PageHeading } from "src/components/Layout/PageHeading";
+import { SideMenu } from "src/components/Layout/SideMenu";
 
 const Genres: NextPage = () => {
   return (
@@ -9,8 +11,15 @@ const Genres: NextPage = () => {
       <Head>
         <title>ジャンル一覧 - FilmWorld</title>
       </Head>
-      <PageHeading text="ジャンル一覧" />
-      <GenreList />
+      <Grid container spacing={1} columns={{ xs: 4, sm: 8 }}>
+        <Grid item xs={0} sm={2} sx={{ display: { xs: "none", sm: "flex" } }}>
+          <SideMenu />
+        </Grid>
+        <Grid item xs={4} sm={6}>
+          <PageHeading text="ジャンル一覧" />
+          <GenreList />
+        </Grid>
+      </Grid>
     </div>
   );
 };
