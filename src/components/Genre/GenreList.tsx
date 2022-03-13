47cb34e2b6569555234b88c5e5a397a3210e97d5
capcap-jp <miyamiyacap@gmail.com>
2022-03-13 22:45:28 +0900

    💄 モバイル画面用のリスト表示を追加

--- src/components/Genre/GenreList.tsx
+++ src/components/Genre/GenreList.tsx
@@ -1,11 +1,24 @@
 import NextLink from "next/link";
 import { VFC } from "react";
-import { useGenres } from "src/hooks/useGenres";
+import { useMediaQuery } from "react-responsive";
+import {
+  Box,
+  Chip,
+  Grid,
+  IconButton,
+  Link as MuiLink,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import { Loading } from "src/components/Layout/Loading";
-import { Chip, Grid, Link as MuiLink } from "@mui/material";
+import { useGenres } from "src/hooks/useGenres";
 
 export const GenreList: VFC = () => {
   const { genres, genresError, isLoading } = useGenres();
+  const isMobileScreen = useMediaQuery({ query: "(max-width: 600px)" });
 
   if (isLoading) {
     return <Loading />;
@@ -16,31 +29,64 @@ export const GenreList: VFC = () => {
   }
 
   return (
-    <Grid
-      container
-      columns={{ xs: 4, sm: 6 }}
-      justifyContent="center"
-      spacing={2}
-    >
-      {genres?.genres.map((genre: { id: number; name: string }) => {
-        return (
-          <Grid item key={genre.id} xs="auto" sm={2}>
-            <NextLink
-              href={`/movies/genre?id=${genre.id}&sort=popularity.desc&page=1`}
-              passHref
-            >
-              <MuiLink underline="none">
-                <Chip
-                  clickable
-                  color="primary"
-                  label={genre.name}
-                  variant="outlined"
-                />
-              </MuiLink>
-            </NextLink>
-          </Grid>
-        );
-      })}
-    </Grid>
+    <>
+      {isMobileScreen ? (
+        <Box sx={{ width: "100%" }}>
+          <List sx={{ pt: 0 }}>
+            {genres?.genres.map((genre: { id: number; name: string }) => {
+              return (
+                <NextLink
+                  href={`/movies/genre?id=${genre.id}&sort=popularity.desc&page=1`}
+                  passHref
+                >
+                  <MuiLink color="white" underline="none">
+                    <ListItem
+                      dense
+                      disableGutters
+                      secondaryAction={
+                        <IconButton edge="end">
+                          <ChevronRightIcon />
+                        </IconButton>
+                      }
+                    >
+                      <ListItemButton>
+                        <ListItemText>{genre.name}</ListItemText>
+                      </ListItemButton>
+                    </ListItem>
+                  </MuiLink>
+                </NextLink>
+              );
+            })}
+          </List>
+        </Box>
+      ) : (
+        <Grid
+          container
+          columns={{ xs: 4, sm: 6 }}
+          justifyContent="center"
+          spacing={2}
+        >
+          {genres?.genres.map((genre: { id: number; name: string }) => {
+            return (
+              <Grid item key={genre.id} xs="auto" sm={2}>
+                <NextLink
+                  href={`/movies/genre?id=${genre.id}&sort=popularity.desc&page=1`}
+                  passHref
+                >
+                  <MuiLink underline="none">
+                    <Chip
+                      clickable
+                      color="primary"
+                      label={genre.name}
+                      variant="outlined"
+                    />
+                  </MuiLink>
+                </NextLink>
+              </Grid>
+            );
+          })}
+        </Grid>
+      )}
+    </>
   );
 };
