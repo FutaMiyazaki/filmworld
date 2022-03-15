d241516d2f0ddc9e8040717f5ecebe8de4356b74
capcap-jp <miyamiyacap@gmail.com>
2022-03-16 08:49:08 +0900

    ♻️ 固定のパスおよびクエリパラメータを定数で置き換え

--- src/hooks/topMovies/usePopularMovies.tsx
+++ src/hooks/topMovies/usePopularMovies.tsx
@@ -2,8 +2,10 @@ import useSWR from "swr";
 import { fetcher } from "src/utils/fetcher";
 
 export const usePopularMovies = () => {
+  const API_URL = "https://api.themoviedb.org/3";
+  const API_KEY = "api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca";
   const { data: movies, error } = useSWR(
-    "https://api.themoviedb.org/3/movie/popular?api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca&language=ja-JP&page=1&region=JP",
+    `${API_URL}/movie/popular?${API_KEY}&language=ja-JP&region=JP&page=1`,
     fetcher
   );
 
