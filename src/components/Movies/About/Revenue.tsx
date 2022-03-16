4dd4ff044743a5ce5c02f90c577717d2ca95ee4e
capcap-jp <miyamiyacap@gmail.com>
2022-03-16 23:17:07 +0900

    ✨ 表示デザインを修正

--- src/components/Movies/About/Revenue.tsx
+++ src/components/Movies/About/Revenue.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 export const Revenue = ({ revenue = 0 }: { revenue: number }) => {
   const fmtRevenue = new Intl.NumberFormat("ja-JP", {
@@ -8,9 +8,18 @@ export const Revenue = ({ revenue = 0 }: { revenue: number }) => {
   return (
     <>
       {revenue ? (
-        <Typography color="white" variant="body1" sx={{ mb: 1 }}>
-          興行収入： {fmtRevenue}ドル
-        </Typography>
+        <Box sx={{ mb: 1 }}>
+          <Typography
+            color="white"
+            variant="subtitle1"
+            sx={{ display: "inline", fontWeight: "bold", mr: 2 }}
+          >
+            興行収入
+          </Typography>
+          <Typography color="white" variant="body2" sx={{ display: "inline" }}>
+            {fmtRevenue}円
+          </Typography>
+        </Box>
       ) : null}
     </>
   );
