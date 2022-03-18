8d10bcc0be4605fa4848359af86f9633583f592b
capcap-jp <miyamiyacap@gmail.com>
2022-03-19 03:39:43 +0900

    💄 もっと見るボタンを追加

--- src/components/Movies/About/CastInfo.tsx
+++ src/components/Movies/About/CastInfo.tsx
@@ -1,6 +1,12 @@
 import NextLink from "next/link";
-import { VFC } from "react";
-import { Box, Link as MuiLink, Typography } from "@mui/material";
+import { useState, VFC } from "react";
+import {
+  Box,
+  Button,
+  Collapse,
+  Link as MuiLink,
+  Typography,
+} from "@mui/material";
 import { InfoHeader } from "src/components/Movies/About/InfoHeader";
 
 type CastData = {
@@ -13,7 +19,12 @@ type CastProps = {
 };
 
 export const CastInfo: VFC<CastProps> = (props) => {
+  const [open, setOpen] = useState(false);
   const { cast } = props;
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
 
   return (
     <>
@@ -40,6 +51,38 @@ export const CastInfo: VFC<CastProps> = (props) => {
               </NextLink>
             ) : null;
           })}
+          {!open ? (
+            <Button
+              fullWidth
+              onClick={handleOpen}
+              size="small"
+              sx={{ display: "block", mt: 1 }}
+            >
+              すべて表示
+            </Button>
+          ) : null}
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            {cast?.map(({ id, name }, i: number) => {
+              return i > 10 && i < 30 ? (
+                <NextLink
+                  key={id}
+                  href={`/movies/cast?id=${id}&sort=popularity.desc&page=1`}
+                  passHref
+                >
+                  <MuiLink underline="hover" sx={{ display: "inline-block" }}>
+                    <Typography
+                      variant="body2"
+                      sx={{
+                        mr: 2,
+                      }}
+                    >
+                      {name}
+                    </Typography>
+                  </MuiLink>
+                </NextLink>
+              ) : null;
+            })}
+          </Collapse>
         </Box>
       ) : null}
     </>
