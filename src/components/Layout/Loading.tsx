e13a9a684385038db7142993c9f1e37cd082386c
capcap-jp <miyamiyacap@gmail.com>
2022-03-20 20:56:28 +0900

    💄 余白を修正

--- src/components/Layout/Loading.tsx
+++ src/components/Layout/Loading.tsx
@@ -5,7 +5,7 @@ export const Loading: VFC = () => {
   return (
     <Box>
       <Grid container alignItems="center" justifyContent="center">
-        <CircularProgress size={80} sx={{ mt: 20 }} />
+        <CircularProgress size={80} sx={{ mt: 10 }} />
       </Grid>
     </Box>
   );
