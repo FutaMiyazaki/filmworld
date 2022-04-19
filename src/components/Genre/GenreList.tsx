59e4a34db5d0de8ecc5637340ea914379d608845
capcap-jp <miyamiyacap@gmail.com>
2022-04-20 01:36:36 +0900

    💄 ジャンル一覧の表示レイアウトを変更

--- src/components/Genre/GenreList.tsx
+++ src/components/Genre/GenreList.tsx
@@ -1,26 +1,11 @@
-import NextLink from "next/link";
 import { VFC } from "react";
-import { useMediaQuery } from "react-responsive";
-import {
-  Box,
-  Chip,
-  Grid,
-  IconButton,
-  Link as MuiLink,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemText,
-  Paper,
-  Typography,
-} from "@mui/material";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { Grid, Paper, Typography } from "@mui/material";
+import { AppLink } from "src/components/Layout/Link/AppLink";
 import { Loading } from "src/components/Layout/Loading";
 import { useGenres } from "src/hooks/useGenres";
 
 export const GenreList: VFC = () => {
   const { genres, genresError, isLoading } = useGenres();
-  const isMobileScreen = useMediaQuery({ query: "(max-width: 600px)" });
 
   if (isLoading) {
     return <Loading />;
@@ -31,65 +16,29 @@ export const GenreList: VFC = () => {
   }
 
   return (
-    <>
-      {isMobileScreen ? (
-        <Box sx={{ width: "100%" }}>
-          <List sx={{ pt: 0 }}>
-            {genres?.genres.map((genre: { id: number; name: string }) => {
-              return (
-                <NextLink
-                  key={genre.id}
-                  href={`/movies/genre?id=${genre.id}&sort=popularity.desc&page=1`}
-                  passHref
-                >
-                  <MuiLink color="white" underline="none">
-                    <ListItem
-                      dense
-                      disableGutters
-                      secondaryAction={
-                        <IconButton edge="end">
-                          <ChevronRightIcon />
-                        </IconButton>
-                      }
-                    >
-                      <ListItemButton>
-                        <ListItemText>{genre.name}</ListItemText>
-                      </ListItemButton>
-                    </ListItem>
-                  </MuiLink>
-                </NextLink>
-              );
-            })}
-          </List>
-        </Box>
-      ) : (
-        <Grid container columns={{ sm: 6, lg: 8 }} spacing={2}>
-          {genres?.genres.map((genre: { id: number; name: string }) => {
-            return (
-              <Grid item key={genre.id} sm={2} lg={2}>
-                <NextLink
-                  href={`/movies/genre?id=${genre.id}&sort=popularity.desc&page=1`}
-                  passHref
-                >
-                  <MuiLink underline="none">
-                    <Paper
-                      sx={{
-                        textAlign: "center",
-                        p: 1,
-                        "&:hover": {
-                          opacity: 0.5,
-                        },
-                      }}
-                    >
-                      <Typography variant="body1">{genre.name}</Typography>
-                    </Paper>
-                  </MuiLink>
-                </NextLink>
-              </Grid>
-            );
-          })}
-        </Grid>
-      )}
-    </>
+    <Grid container columns={{ sm: 3, md: 4, lg: 5 }} spacing={2}>
+      {genres?.genres.map((genre: { id: number; name: string }) => {
+        return (
+          <Grid item key={genre.id} sm={1} md={1} lg={1}>
+            <AppLink
+              path={`/movies/genre?id=${genre.id}&sort=popularity.desc&page=1`}
+              underline="none"
+            >
+              <Paper
+                sx={{
+                  textAlign: "center",
+                  p: 1,
+                  "&:hover": {
+                    opacity: 0.5,
+                  },
+                }}
+              >
+                <Typography variant="body1">{genre.name}</Typography>
+              </Paper>
+            </AppLink>
+          </Grid>
+        );
+      })}
+    </Grid>
   );
 };
--- src/pages/genres/index.tsx
+++ src/pages/genres/index.tsx
@@ -1,9 +1,7 @@
 import { NextPage } from "next";
 import Head from "next/head";
-import { Grid } from "@mui/material";
 import { GenreList } from "src/components/Genre/GenreList";
 import { PageHeading } from "src/components/Layout/PageHeading";
-import { SideMenu } from "src/components/Layout/SideMenu";
 
 const Genres: NextPage = () => {
   return (
@@ -11,15 +9,8 @@ const Genres: NextPage = () => {
       <Head>
         <title>ジャンル一覧 - FilmWorld</title>
       </Head>
-      <Grid container spacing={1} columns={{ xs: 4, sm: 8 }}>
-        <Grid item xs={0} sm={2} sx={{ display: { xs: "none", sm: "flex" } }}>
-          <SideMenu />
-        </Grid>
-        <Grid item xs={4} sm={6}>
-          <PageHeading text="ジャンル一覧" />
-          <GenreList />
-        </Grid>
-      </Grid>
+      <PageHeading text="ジャンル一覧" />
+      <GenreList />
     </div>
   );
 };
