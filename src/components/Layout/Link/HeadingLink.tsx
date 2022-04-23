722264de7b8924895a4b07754f0d75d8e5c15564
capcap-jp <miyamiyacap@gmail.com>
2022-04-23 22:39:25 +0900

    💄 リンクをもつ見出しのコンポーネントを追加

---
+++ src/components/Layout/Link/HeadingLink.tsx
@@ -0,0 +1,23 @@
+import { VFC } from "react";
+import { Box, Typography } from "@mui/material";
+import { AppLink } from "src/components/Layout/Link/AppLink";
+
+type HeadingLinkProps = {
+  path: string;
+  text: string;
+  underline: "always" | "hover" | "none";
+};
+
+export const HeadingLink: VFC<HeadingLinkProps> = (props) => {
+  const { path, text, underline } = props;
+
+  return (
+    <Box sx={{ mb: 2 }}>
+      <AppLink path={path} underline={underline}>
+        <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+          {text}
+        </Typography>
+      </AppLink>
+    </Box>
+  );
+};
