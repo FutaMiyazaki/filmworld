460985a9cf3ac83765c116d094fe95faa91a87ef
capcap-jp <miyamiyacap@gmail.com>
2022-04-24 04:09:58 +0900

    💄 表示レイアウトを修正

--- src/components/Layout/Link/RankingButtonLinks.tsx
+++ src/components/Layout/Link/RankingButtonLinks.tsx
@@ -1,33 +1,27 @@
 import { useRouter } from "next/router";
 import { VFC } from "react";
-import { useMediaQuery } from "react-responsive";
-import { Box, Button, ButtonGroup, Typography } from "@mui/material";
+import { Button, ButtonGroup, Typography } from "@mui/material";
 import { AppLink } from "src/components/Layout/Link/AppLink";
 
 type ButtonItems = {
   path: string;
   text: string;
-  mobileText: string;
 }[];
 
 export const RankingButtonLinks: VFC = () => {
   const router = useRouter();
-  const isMobileScreen = useMediaQuery({ query: "(max-width: 600px)" });
   const buttonItems: ButtonItems = [
     {
       path: "/movies/popular?page=1",
-      text: "人気ランキング",
-      mobileText: "人気",
+      text: "人気",
     },
     {
       path: "/movies/revenue?page=1",
-      text: "興行収入ランキング",
-      mobileText: "興行収入",
+      text: "興行収入",
     },
     {
       path: "/movies/topic?page=1",
-      text: "レビュー数ランキング",
-      mobileText: "レビュー数",
+      text: "レビュー数",
     },
   ];
 
@@ -38,29 +32,22 @@ export const RankingButtonLinks: VFC = () => {
         variant="caption"
         sx={{ fontWeight: "bold" }}
       >
-        並び替え：
+        並べ替え：
       </Typography>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "flex-end",
-        }}
-      >
-        <ButtonGroup size={isMobileScreen ? "small" : "medium"} variant="text">
-          {buttonItems.map(({ path, text, mobileText }) => {
-            return (
-              <AppLink key={path} path={path} underline="none">
-                <Button
-                  disabled={`${router.pathname}?page=1` === path}
-                  sx={{ mr: 1, fontWeight: "bold" }}
-                >
-                  {isMobileScreen ? mobileText : text}
-                </Button>
-              </AppLink>
-            );
-          })}
-        </ButtonGroup>
-      </Box>
+      <ButtonGroup variant="text">
+        {buttonItems.map(({ path, text }) => {
+          return (
+            <AppLink key={path} path={path} underline="none">
+              <Button
+                disabled={`${router.pathname}?page=1` === path}
+                sx={{ mr: 1, fontWeight: "bold" }}
+              >
+                {text}
+              </Button>
+            </AppLink>
+          );
+        })}
+      </ButtonGroup>
     </>
   );
 };
