f597af69c873d81fcdddb4796718144f04e4d67f
capcap-jp <miyamiyacap@gmail.com>
2022-04-23 22:40:23 +0900

    ♻️ refactoring

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,22 @@
 import { NextPage } from "next";
-import { PageHeading } from "src/components/Layout/PageHeading";
+import { HeadingLink } from "src/components/Layout/Link/HeadingLink";
 import { MoviesTopPopular } from "src/components/Movies/Top/Popular";
 import { MoviesTopRevenue } from "src/components/Movies/Top/Revenue";
 
 const Index: NextPage = () => {
   return (
     <div>
-      <PageHeading path="/movies/popular?page=1" text="人気ランキング" />
+      <HeadingLink
+        path="/movies/popular?page=1"
+        text="人気ランキング"
+        underline="hover"
+      />
       <MoviesTopPopular />
-      <PageHeading path="/movies/popular?page=1" text="興行収入ランキング" />
+      <HeadingLink
+        path="/movies/revenue?page=1"
+        text="興行収入ランキング"
+        underline="hover"
+      />
       <MoviesTopRevenue />
     </div>
   );
