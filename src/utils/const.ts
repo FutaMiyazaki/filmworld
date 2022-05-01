f70ae7cfe35204034d0cc4dcf9fcc276f0a66f51
capcap-jp <miyamiyacap@gmail.com>
2022-05-01 16:28:32 +0900

    ♻️ ジャンル及び制作会社に関する配列を追加

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -14,3 +14,26 @@ export const SORT_TYPES = [
     text: "レビュー数",
   },
 ];
+export const COMPANY_LIST = [
+  { id: "2", text: "ディズニー" },
+  { id: "33", text: "ユニバーサル" },
+  { id: "174", text: "ワーナー・ブラザーズ" },
+  { id: "25", text: "20世紀スタジオ" },
+  { id: "4", text: "パラマウント" },
+  { id: "420", text: "MARVEL" },
+  { id: "3", text: "Pixar" },
+  { id: "882", text: "東宝" },
+  { id: "192", text: "松竹" },
+];
+export const GENRE_LIST = [
+  { id: "28", text: "アクション" },
+  { id: "12", text: "アドベンチャー" },
+  { id: "16", text: "アニメーション" },
+  { id: "35", text: "コメディ" },
+  { id: "99", text: "ドキュメンタリー" },
+  { id: "10751", text: "ファミリー" },
+  { id: "14", text: "ファンタジー" },
+  { id: "27", text: "ホラー" },
+  { id: "10749", text: "ロマンス" },
+  { id: "878", text: "サイエンスフィクション" },
+];
