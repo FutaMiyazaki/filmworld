89ae9968fa946c9d465706e09a00620540668185
capcap-jp <miyamiyacap@gmail.com>
2022-05-01 16:04:14 +0900

    ♻️ 並び順に関する配列を追加

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,2 +1,16 @@
 export const API_URL = "https://api.themoviedb.org/3";
 export const API_KEY = "api_key=a9f5f6a6a7d86b9c7a665290b1dc19ca";
+export const SORT_TYPES = [
+  {
+    sort: "popularity.desc",
+    text: "人気",
+  },
+  {
+    sort: "revenue.desc",
+    text: "興行収入",
+  },
+  {
+    sort: "vote_count.desc",
+    text: "レビュー数",
+  },
+];
